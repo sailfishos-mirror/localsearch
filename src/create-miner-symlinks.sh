5bebbcf8b2928b39eab566d7cbe84eb3f8761ae4
Carlos Garnacho <carlosg@gnome.org>
2017-08-03 17:56:23 +0200

    meson: Create all miner links on $datadir/tracker/miners

--- src/create-miner-symlinks.sh
+++ src/create-miner-symlinks.sh
@@ -10,4 +10,7 @@ dbus_services_dir="$1"
 tracker_miner_services_dir="$2"
 
 mkdir -p ${DESTDIR}/${tracker_miner_services_dir}
-ln -sf "${dbus_services_dir}/tracker-extract.service" "${DESTDIR}/${tracker_miner_services_dir}/tracker-extract.service"
+ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Applications.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Extract.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Files.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.RSS.service" "${DESTDIR}/${tracker_miner_services_dir}/"
