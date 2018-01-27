GNOME/tinysparql

commit 5c04c7dedeb985db539d781670ba6b31cfa2006a

tracker-extract: Fixes NB#161047: X/Y resolution of images

src/tracker-extract/tracker-extract-jpeg.c
@@ -35,6 +35,8 @@
 
 #include "tracker-main.h"
 
+#define CM_TO_INCH              0.393700787
+
 #ifdef HAVE_LIBEXIF
 #define EXIF_NAMESPACE          "Exif"
 #define EXIF_NAMESPACE_LENGTH   4
@@ -572,20 +574,42 @@ extract_jpeg (const gchar          *uri,
 		tracker_sparql_builder_object_blank_close (metadata);
 	}
 
-	if (ed->x_resolution) {
+	if (cinfo.density_unit != 0 || ed->x_resolution) {
 		gdouble value;
 
-		value = g_strtod (ed->x_resolution, NULL);
+		if (cinfo.density_unit == 0) {
+			if (ed->resolution_unit == 1)
+				value = g_strtod (ed->x_resolution, NULL);
+			else
+				value = g_strtod (ed->x_resolution, NULL) * CM_TO_INCH;
+		} else {
+			if (cinfo.density_unit == 1)
+				value = cinfo.X_density;
+			else
+				value = cinfo.X_density * CM_TO_INCH;
+		}
+
 		tracker_sparql_builder_predicate (metadata, "nfo:horizontalResolution");
-		tracker_sparql_builder_object_int64 (metadata, (gint64) value);
+		tracker_sparql_builder_object_double (metadata, value);
 	}
 
-	if (ed->y_resolution) {
+	if (cinfo.density_unit != 0 || ed->y_resolution) {
 		gdouble value;
 
-		value = g_strtod (ed->y_resolution, NULL);
+		if (cinfo.density_unit == 0) {
+			if (ed->resolution_unit == 1)
+				value = g_strtod (ed->y_resolution, NULL);
+			else
+				value = g_strtod (ed->y_resolution, NULL) * CM_TO_INCH;
+		} else {
+			if (cinfo.density_unit == 1)
+				value = cinfo.Y_density;
+			else
+				value = cinfo.Y_density * CM_TO_INCH;
+		}
+
 		tracker_sparql_builder_predicate (metadata, "nfo:verticalResolution");
-		tracker_sparql_builder_object_int64 (metadata, (gint64) value);
+		tracker_sparql_builder_object_double (metadata, value);
 	}
 
 	jpeg_destroy_decompress (&cinfo);

src/tracker-extract/tracker-extract-png.c
@@ -31,6 +31,7 @@
 #include <libtracker-extract/tracker-extract.h>
 
 #define RFC1123_DATE_FORMAT "%d %B %Y %H:%M:%S %z"
+#define CM_TO_INCH          0.393700787
 
 typedef struct {
 	const gchar *title;
@@ -468,6 +469,23 @@ read_metadata (TrackerSparqlBuilder *preupdate,
 		tracker_sparql_builder_object_blank_close (metadata);
 	}
 
+	if (ed->x_resolution) {
+		gdouble value;
+
+		value = ed->resolution_unit == 1 ? g_strtod (ed->x_resolution, NULL) : g_strtod (ed->x_resolution, NULL) * CM_TO_INCH;
+		tracker_sparql_builder_predicate (metadata, "nfo:horizontalResolution");
+		tracker_sparql_builder_object_double (metadata, value);
+	}
+
+	if (ed->y_resolution) {
+		gdouble value;
+
+		value = ed->resolution_unit == 1 ? g_strtod (ed->y_resolution, NULL) : g_strtod (ed->y_resolution, NULL) * CM_TO_INCH;
+		tracker_sparql_builder_predicate (metadata, "nfo:verticalResolution");
+		tracker_sparql_builder_object_double (metadata, value);
+	}
+
+	tracker_exif_free (ed);
 	tracker_xmp_free (xd);
 	g_free (pd.creation_time);
 	g_free (md.camera);

src/tracker-extract/tracker-extract-tiff.c
@@ -27,6 +27,8 @@
 #include <libtracker-client/tracker-client.h>
 #include <libtracker-extract/tracker-extract.h>
 
+#define CM_TO_INCH          0.393700787
+
 typedef enum {
 	TAG_TYPE_UNDEFINED = 0,
 	TAG_TYPE_STRING,
@@ -674,6 +676,22 @@ extract_tiff (const gchar          *uri,
 		tracker_sparql_builder_predicate (metadata, "nco:creator");
 		tracker_sparql_builder_object_iri (metadata, uri);
 		g_free (uri);
+	}
+
+	if (ed->x_resolution) {
+		gdouble value;
+
+		value = ed->resolution_unit == 1 ? g_strtod (ed->x_resolution, NULL) : g_strtod (ed->x_resolution, NULL) * CM_TO_INCH;
+		tracker_sparql_builder_predicate (metadata, "nfo:horizontalResolution");
+		tracker_sparql_builder_object_double (metadata, value);
+	}
+
+	if (ed->y_resolution) {
+		gdouble value;
+
+		value = ed->resolution_unit == 1 ? g_strtod (ed->y_resolution, NULL) : g_strtod (ed->y_resolution, NULL) * CM_TO_INCH;
+		tracker_sparql_builder_predicate (metadata, "nfo:verticalResolution");
+		tracker_sparql_builder_object_double (metadata, value);
 	}
 
 	g_free (md.camera);

src/libtracker-extract/tracker-exif.c
@@ -278,6 +278,24 @@ get_white_balance (ExifData *exif,
 	return NULL;
 }
 
+
+static gint
+get_int (ExifData *exif, 
+         ExifTag   tag)
+{
+	ExifEntry *entry = exif_data_get_entry (exif, tag);
+
+	if (entry) {
+		ExifByteOrder order;
+
+		order = exif_data_get_byte_order (exif);
+		return (gint) exif_get_short (entry->data, order);
+	}
+
+	return -1;
+}
+
+
 static gchar *
 get_value (ExifData *exif, 
            ExifTag   tag)
@@ -366,6 +384,9 @@ parse_exif (const unsigned char *buffer,
 		data->copyright = get_value (exif, EXIF_TAG_COPYRIGHT);
 	if (!data->software)
 		data->software = get_value (exif, EXIF_TAG_SOFTWARE);
+
+	if (!data->resolution_unit)
+		data->resolution_unit = get_int (exif, EXIF_TAG_RESOLUTION_UNIT);
 	if (!data->x_resolution)
 		data->x_resolution = get_value (exif, EXIF_TAG_X_RESOLUTION);
 	if (!data->y_resolution)

src/libtracker-extract/tracker-exif.h
@@ -52,6 +52,7 @@ typedef struct {
 	gchar *software;
 	gchar *x_resolution;
 	gchar *y_resolution;
+	gint resolution_unit;
 } TrackerExifData;
 
 #ifndef TRACKER_DISABLE_DEPRECATED

sailfishos-mirror/localsearch

commit 8ef8b3b8bcff20a8b78bad2390f0027ff3088814

tracker-extract: Fix pixel density conversions
The CM_TO_INCH factor is meant to convert pixels per centimetre to
pixels per inch, not centimetres to inches. If there is one pixel per
centimetre, then there would be 2.54 pixels per inch.

Fallout from 5c04c7dedeb985db539d781670ba6b31cfa2006a

https://bugzilla.gnome.org/show_bug.cgi?id=792192

src/tracker-extract/tracker-extract-jpeg.c
@@ -31,7 +31,7 @@
 
 #include "tracker-main.h"
 
-#define CM_TO_INCH              0.393700787
+#define CM_TO_INCH              2.54
 
 #ifdef HAVE_LIBEXIF
 #define EXIF_NAMESPACE          "Exif"

src/tracker-extract/tracker-extract-png.c
@@ -27,7 +27,7 @@
 #include <libtracker-extract/tracker-extract.h>
 
 #define RFC1123_DATE_FORMAT "%d %B %Y %H:%M:%S %z"
-#define CM_TO_INCH          0.393700787
+#define CM_TO_INCH          2.54
 
 typedef struct {
 	const gchar *title;

src/tracker-extract/tracker-extract-tiff.c
@@ -33,7 +33,7 @@
 #include <libtracker-miners-common/tracker-common.h>
 #include <libtracker-extract/tracker-extract.h>
 
-#define CM_TO_INCH          0.393700787
+#define CM_TO_INCH          2.54
 
 typedef enum {
 	TAG_TYPE_UNDEFINED = 0,