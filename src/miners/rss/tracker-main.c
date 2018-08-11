5a58a451dfa6f1129402f5c7bd55fcd137369930
Sam Thursfield <sam@afuera.me.uk>
2018-08-11 20:52:16 +0200

    Allow use of domain rules that aren't installed into /usr
    Currently Tracker domain rules must be installed inside Tracker's data
    directory (usually /usr/share/tracker). This is limiting as it means
    only system packages can add them. A program installed into /opt is
    unable to use a custom domain, for example. Since Tracker is implemented
    as a system of daemons, it's not particularly straight forward to work
    around this by setting XDG_DATA_DIRS= to point somewhere non-standard
    either.
    
    This patch removes this restriction in a simple way: it allows users to
    pass a full path to the domain rule, rather than just the base name.

--- src/miners/rss/tracker-main.c
+++ src/miners/rss/tracker-main.c
@@ -79,7 +79,7 @@ main (int argc, char **argv)
 	GDBusConnection *connection;
 	TrackerMinerProxy *proxy;
 	TrackerDomainOntology *domain_ontology;
-	gchar *dbus_name;
+	gchar *domain_name, *dbus_name;
 
 	setlocale (LC_ALL, "");
 
@@ -217,11 +217,13 @@ main (int argc, char **argv)
 
 	loop = g_main_loop_new (NULL, FALSE);
 
-	if (domain_ontology && domain_ontology_name) {
+	if (domain_ontology) {
+		domain_name = tracker_domain_ontology_get_domain (domain_ontology, NULL);
 		g_bus_watch_name_on_connection (connection, domain_ontology_name,
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                loop, NULL);
+		g_free (domain_name);
 	}
 
 	g_main_loop_run (loop);
