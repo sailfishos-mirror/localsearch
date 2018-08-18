53990f24e91143e76fe66f5b00a9bab8a4f1dd72
Jan Tojnar <jtojnar@gmail.com>
2018-08-18 17:16:55 +0200

    meson: Do not symlink disabled services
    https://bugzilla.gnome.org/show_bug.cgi?id=796145

--- src/create-miner-symlinks.sh
+++ src/create-miner-symlinks.sh
@@ -8,9 +8,18 @@ set -e
 
 dbus_services_dir="$1"
 tracker_miner_services_dir="$2"
+have_tracker_miner_apps="$3"
+have_tracker_miner_fs="$4"
+have_tracker_miner_rss="$5"
 
 mkdir -p ${DESTDIR}/${tracker_miner_services_dir}
-ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Applications.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+if [[ "$have_tracker_miner_apps" -eq "true" ]]; then
+  ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Applications.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+fi
 ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Extract.service" "${DESTDIR}/${tracker_miner_services_dir}/"
-ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Files.service" "${DESTDIR}/${tracker_miner_services_dir}/"
-ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.RSS.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+if [[ "$have_tracker_miner_fs" -eq "true" ]]; then
+  ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.Files.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+fi
+if [[ "$have_tracker_miner_rss" -eq "true" ]]; then
+  ln -sf "${dbus_services_dir}/org.freedesktop.Tracker1.Miner.RSS.service" "${DESTDIR}/${tracker_miner_services_dir}/"
+fi
