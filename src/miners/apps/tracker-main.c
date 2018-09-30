sailfishos-mirror/localsearch

commit 8b6bc5b71f7be3af55b1f3ad18f0d8c9612a37a8

miners: Unbreak domain ontology owner name watch
Commit ab29a9cd6af got it wrong. This is not a tracker-store watch,
but a watch on the domain ontology owner. It should only apply when
there is one such.

src/miners/apps/tracker-main.c
@@ -248,9 +248,8 @@ main (gint argc, gchar *argv[])
 
 	main_loop = g_main_loop_new (NULL, FALSE);
 
-	if (domain_ontology) {
-		domain_name = tracker_domain_ontology_get_domain (domain_ontology, "Tracker1");
-		g_bus_watch_name_on_connection (connection, domain_name,
+	if (domain_ontology && domain_ontology_name) {
+		g_bus_watch_name_on_connection (connection, domain_ontology_name,
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                main_loop, NULL);

src/miners/fs/tracker-main.c
@@ -749,9 +749,8 @@ main (gint argc, gchar *argv[])
 
 	main_loop = g_main_loop_new (NULL, FALSE);
 
-	if (domain_ontology) {
-		domain_name = tracker_domain_ontology_get_domain (domain_ontology, "Tracker1");
-		g_bus_watch_name_on_connection (connection, domain_name,
+	if (domain_ontology && domain_ontology_name) {
+		g_bus_watch_name_on_connection (connection, domain_ontology_name,
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                main_loop, NULL);

src/miners/rss/tracker-main.c
@@ -217,8 +217,7 @@ main (int argc, char **argv)
 
 	loop = g_main_loop_new (NULL, FALSE);
 
-	if (domain_ontology) {
-		domain_name = tracker_domain_ontology_get_domain (domain_ontology, "Tracker1");
+	if (domain_ontology && domain_ontology_name) {
 		g_bus_watch_name_on_connection (connection, domain_ontology_name,
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,

src/tracker-extract/tracker-main.c
@@ -494,9 +494,8 @@ main (int argc, char *argv[])
 	/* Main loop */
 	main_loop = g_main_loop_new (NULL, FALSE);
 
-	if (domain_ontology) {
-		domain_name = tracker_domain_ontology_get_domain (domain_ontology, "Tracker1");
-		g_bus_watch_name_on_connection (connection, domain_name,
+	if (domain_ontology && domain_ontology_name) {
+		g_bus_watch_name_on_connection (connection, domain_ontology_name,
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                main_loop, NULL);

commit f9e7de0fe0549b009defc68ba37ba2516a03f49c

Fix free of unused memory when --domain-ontology flag is passed
This fixes a regression from 8b6bc5b71f7be3af55b1f3ad18f0d8c9612a37a8.

src/miners/apps/tracker-main.c
@@ -186,7 +186,7 @@ main (gint argc, gchar *argv[])
 	GDBusConnection *connection;
 	TrackerMinerProxy *proxy;
 	TrackerDomainOntology *domain_ontology;
-	gchar *domain_name, *dbus_name;
+	gchar *dbus_name;
 
 	main_loop = NULL;
 
@@ -253,7 +253,6 @@ main (gint argc, gchar *argv[])
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                main_loop, NULL);
-		g_free (domain_name);
 	}
 
 	g_message ("Checking if we're running as a daemon:");

src/miners/fs/tracker-main.c
@@ -667,7 +667,7 @@ main (gint argc, gchar *argv[])
 	TrackerMinerProxy *proxy;
 	GDBusConnection *connection;
 	TrackerDomainOntology *domain_ontology;
-	gchar *domain_name, *dbus_name;
+	gchar *dbus_name;
 
 	main_loop = NULL;
 
@@ -754,7 +754,6 @@ main (gint argc, gchar *argv[])
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                main_loop, NULL);
-		g_free (domain_name);
 	}
 
 	g_message ("Checking if we're running as a daemon:");

src/miners/rss/tracker-main.c
@@ -79,7 +79,7 @@ main (int argc, char **argv)
 	GDBusConnection *connection;
 	TrackerMinerProxy *proxy;
 	TrackerDomainOntology *domain_ontology;
-	gchar *domain_name, *dbus_name;
+	gchar *dbus_name;
 
 	setlocale (LC_ALL, "");
 
@@ -222,7 +222,6 @@ main (int argc, char **argv)
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                loop, NULL);
-		g_free (domain_name);
 	}
 
 	g_main_loop_run (loop);

src/tracker-extract/tracker-main.c
@@ -351,7 +351,7 @@ main (int argc, char *argv[])
 	GDBusConnection *connection;
 	TrackerMinerProxy *proxy;
 	TrackerDomainOntology *domain_ontology;
-	gchar *domain_name, *dbus_name;
+	gchar *dbus_name;
 
 	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
@@ -499,7 +499,6 @@ main (int argc, char *argv[])
 		                                G_BUS_NAME_WATCHER_FLAGS_NONE,
 		                                NULL, on_domain_vanished,
 		                                main_loop, NULL);
-		g_free (domain_name);
 	}
 
 	g_signal_connect (decorator, "finished",