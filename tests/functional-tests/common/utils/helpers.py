GNOME/tracker-miners

functional-tests: Remove a double-traceback
Before:

    Traceback (most recent call last):
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/helpers.py", line 403, in await_resource_inserted
        self.loop.run_checked()
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/mainloop.py", line 58, in run_checked
        raise self._exception
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/helpers.py", line 294, in _graph_updated_timeout_cb
        raise GraphUpdateTimeoutException()
    common.utils.helpers.GraphUpdateTimeoutException

    During handling of the above exception, another exception occurred:

    Traceback (most recent call last):
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/minertest.py", line 145, in setUp
        super(CommonTrackerMinerFTSTest, self).setUp()
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/minertest.py", line 82, in setUp
        self.create_test_data()
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/minertest.py", line 117, in create_test_data
        self.tracker.await_resource_inserted(NFO_DOCUMENT, url=self.uri(tf))
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/helpers.py", line 405, in await_resource_inserted
        raise GraphUpdateTimeoutException("Timeout waiting for resource: class %s, URL %s, title %s" % (rdf_class, url, title))
    common.utils.helpers.GraphUpdateTimeoutException: Timeout waiting for resource: class http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#Document, URL file:///home/tracker/tracker-tests/tmp6bkbec7d/test-monitored/dir1/file2.txt, title None

After:

    Traceback (most recent call last):
      File "/builds/GNOME/tracker-miners/tests/functional-tests/common/utils/minertest.py", line 145, in setUp
        super(CommonTrackerMinerFTSTest, self).setUp()
      File "/builds/GNOME/tracker-miner

tests/functional-tests/common/utils/helpers.py
@@ -402,7 +402,7 @@ class StoreHelper (Helper):
             try:
                 self.loop.run_checked()
             except GraphUpdateTimeoutException as e:
-                raise GraphUpdateTimeoutException("Timeout waiting for resource: class %s, URL %s, title %s" % (rdf_class, url, title))
+                raise GraphUpdateTimeoutException("Timeout waiting for resource: class %s, URL %s, title %s" % (rdf_class, url, title)) from e
             self.inserts_match_function = None
 
         self.class_to_track = None

sailfishos-mirror/localsearch

tests/functional-tests/common/utils/helpers.py
@@ -402,7 +402,7 @@ class StoreHelper (Helper):
             try:
                 self.loop.run_checked()
             except GraphUpdateTimeoutException as e:
-                raise GraphUpdateTimeoutException("Timeout waiting for resource: class %s, URL %s, title %s" % (rdf_class, url, title))
+                raise GraphUpdateTimeoutException("Timeout waiting for resource: class %s, URL %s, title %s" % (rdf_class, url, title)) from None
             self.inserts_match_function = None
 
         self.class_to_track = None