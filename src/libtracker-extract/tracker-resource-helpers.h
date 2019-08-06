GNOME/tracker-miners

extract-mp3: Use ExtractReference to store musicbrainz tags
Store MusicBrainz IDs as ExternalResources instead of a list of
properties. This approach is more generic.

Extraction of MusicBrainz IDs is done based on tags used by Picard.
Tags Mapping: https://picard.musicbrainz.org/docs/mappings/

src/libtracker-extract/tracker-resource-helpers.h
@@ -32,6 +32,7 @@ G_BEGIN_DECLS
 TrackerResource *tracker_extract_new_artist (const char *name);
 TrackerResource *tracker_extract_new_contact (const char *fullname);
 TrackerResource *tracker_extract_new_equipment (const char *make, const char *model);
+TrackerResource *tracker_extract_new_external_resource (const char *source, const char *identifier);
 TrackerResource *tracker_extract_new_location (const char *address, const char *state, const char *city, const char *country, const char *gps_altitude, const char *gps_latitude, const char *gps_longitude);
 TrackerResource *tracker_extract_new_music_album_disc (const char *album_title, TrackerResource *album_artist, int disc_number, const char *date);
 TrackerResource *tracker_extract_new_tag (const char *label);

sailfishos-mirror/localsearch

src/libtracker-extract/tracker-resource-helpers.h
@@ -32,6 +32,7 @@ G_BEGIN_DECLS
 TrackerResource *tracker_extract_new_artist (const char *name);
 TrackerResource *tracker_extract_new_contact (const char *fullname);
 TrackerResource *tracker_extract_new_equipment (const char *make, const char *model);
+TrackerResource *tracker_extract_new_external_reference (const char *source_uri, const char *identifier);
 TrackerResource *tracker_extract_new_location (const char *address, const char *state, const char *city, const char *country, const char *gps_altitude, const char *gps_latitude, const char *gps_longitude);
 TrackerResource *tracker_extract_new_music_album_disc (const char *album_title, TrackerResource *album_artist, int disc_number, const char *date);
 TrackerResource *tracker_extract_new_tag (const char *label);