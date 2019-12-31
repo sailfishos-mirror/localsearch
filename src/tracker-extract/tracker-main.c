0a77821bba7162d08a02c706454cd2819107632f
Carlos Garnacho <carlosg@gnome.org>
2019-12-31 01:54:44 +0100

    tracker-extract: Connect to tracker-miner-fs endpoint
    This service completes data in this endpoint, thus must connect
    to it in order to issue updates and check for files missing
    extraction.

--- src/tracker-extract/tracker-main.c
+++ src/tracker-extract/tracker-main.c
@@ -415,7 +415,7 @@ main (int argc, char *argv[])
 
 	tracker_module_manager_load_modules ();
 
-	dbus_name = tracker_domain_ontology_get_domain (domain_ontology, "Files");
+	dbus_name = tracker_domain_ontology_get_domain (domain_ontology, "Tracker1.Miner.Files");
 	sparql_connection = tracker_sparql_connection_bus_new (dbus_name,
 	                                                       NULL, &error);
 
