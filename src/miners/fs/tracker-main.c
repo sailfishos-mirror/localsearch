dea42e6c8a7fbea85d1730ee0171ce5ffcf1240a
Carlos Garnacho <carlosg@gnome.org>
2020-07-04 21:01:25 +0200

    tracker-miner-fs: Restore --no-daemon functionality
    Even with the FIXME, it remains useful to do performance tests.

--- src/miners/fs/tracker-main.c
+++ src/miners/fs/tracker-main.c
@@ -435,8 +435,8 @@ miner_finished_cb (TrackerMinerFS *fs,
 	 * the mainloop and exit.
 	 */
 	if (no_daemon && main_loop) {
-		// FIXME: wait for extractor to finish
-/*		g_main_loop_quit (main_loop);*/
+		/* FIXME: wait for extractor to finish */
+		g_main_loop_quit (main_loop);
 	}
 }
 
