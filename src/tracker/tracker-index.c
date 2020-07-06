GNOME/tracker-miners

cli: Wait for indexing to finish and report status
This changes the default behaviour of `tracker index`, which would
previously tell the tracker-miner-fs to index a file and then exit
immediately, without giving any feedback to the user about whether
indexing succeeded or failed.

The default behaviour is now to wait for indexing to complete and
report any errors which occured. The old behaviour is still available
with the --no-wait option.

A new --monitor mode is also added, which wraps the IndexFileForProcess
functionality. It lets users trigger crawling and monitoring of
locations which aren't configured for automatic indexing.

Fixes https://gitlab.gnome.org/GNOME/tracker/issues/122

src/tracker/tracker-index.c
@@ -27,6 +27,7 @@
 #endif
 
 #include <glib.h>
+#include <glib-unix.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
 #include <locale.h>
@@ -37,24 +38,140 @@
 #include "tracker-miner-manager.h"
 
 static gboolean file_arg;
+static gboolean monitor_mode;
+static gboolean no_wait;
 static gchar **filenames;
 
 static GOptionEntry entries[] = {
 	{ "file", 'f', 0, G_OPTION_ARG_NONE, &file_arg,
 	 N_("Does nothing, provided for compatibility with Tracker 2.0"),
 	 NULL },
+	{ "monitor", 'm', 0, G_OPTION_ARG_NONE, &monitor_mode,
+	  N_("Trigger indexing and wait for CTRL+C, removing the data again on exit."),
+	  NULL },
+	{ "no-wait", 'w', 0, G_OPTION_ARG_NONE, &no_wait,
+	  N_("Don't wait for processing to complete, exit immediately."),
+	  NULL },
 	{ G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &filenames,
 	  N_("FILE"),
 	  N_("FILE") },
 	{ NULL }
 };
 
-static gint
-index_or_reindex_file (void)
+GMainLoop *main_loop;
+
+static void
+print_indexing_status (GFile                 *root,
+                       TrackerIndexingStatus *status)
 {
-	TrackerMinerManager *manager;
-	GError *error = NULL;
+	g_autofree gchar *uri = g_file_get_uri (root);
+	g_autoptr(GList) error_list = NULL;
+
+	error_list = tracker_indexing_status_get_errors (status);
+
+	if (tracker_indexing_status_get_n_indexed_files (status) == 1) {
+		if (error_list) {
+			const gchar *message = error_list->data;
+			/* TRANSLATORS: %s is a URI. */
+			g_print (_("File %s could not be indexed:"), uri);
+			g_print ("%s\n", message);
+		} else {
+			/* TRANSLATORS: %s is a URI. */
+			g_print (_("File %s was processed successfully."), uri);
+			g_print ("\n");
+		}
+	} else {
+		GList *node;
+
+		if (tracker_indexing_status_get_completed (status)) {
+			/* TRANSLATORS: %s is a URI. */
+			g_print (_("Indexing of %s completed."), uri);
+			g_print ("\n");
+		}
+
+		if (error_list) {
+			for (node = error_list; node; node=node->next) {
+				const gchar *message = node->data;
+				g_print ("  * %s\n", message);
+			}
+		}
+
+		g_print (_("%i files were added to the index."),
+		         tracker_indexing_status_get_n_indexed_files (status));
+		g_print ("\n");
+	}
+
+	g_list_free_full (error_list, g_free);
+}
+
+static void
+index_file_cb (GObject      *source_object,
+               GAsyncResult *res,
+               gpointer      user_data)
+{
+	GMainLoop *loop = user_data;
+
+	tracker_miner_manager_index_file_finish (TRACKER_MINER_MANAGER (source_object), res, NULL);
+
+	g_main_loop_quit (loop);
+}
+
+static gboolean
+g_file_is_directory (GFile *path)
+{
+	g_autoptr(GFileInfo) info = NULL;
+	g_autoptr(GError) error = NULL;
+
+	info = g_file_query_info (path,
+	                          G_FILE_ATTRIBUTE_STANDARD_TYPE,
+	                          G_FILE_QUERY_INFO_NONE,
+	                          NULL,
+	                          &error);
+
+	if (error) {
+		g_warning ("Error checking if %s is a directory: %s.",
+		           g_file_peek_path(path), error->message);
+		return FALSE;
+	}
+
+	return (g_file_info_get_file_type (info) == G_FILE_TYPE_DIRECTORY);
+}
+
+static gboolean
+sigterm_cb (gpointer user_data)
+{
+	g_message ("Received signal");
+
+	g_main_loop_quit (user_data);
+
+	return G_SOURCE_REMOVE;
+}
+
+static gint
+index_run (void)
+{
+	g_autoptr(TrackerMinerManager) manager;
+	g_autoptr(GMainLoop) main_loop;
+	g_autoptr(GError) error = NULL;
+	gboolean success = TRUE;
 	gchar **p;
+
+	/* Check we were only passed directories. IndexFileForProcess doesn't work
+	 * for files. */
+	if (monitor_mode) {
+		for (p = filenames; *p; p++) {
+			g_autoptr(GFile) path = NULL;
+
+			path = g_file_new_for_commandline_arg (*p);
+
+			if (!g_file_is_directory (path)) {
+				g_printerr (_("Could not index file %s: in `--monitor` mode, "
+				              "only directories can be indexed.\n"),
+				            g_file_peek_path (path));
+				return EXIT_FAILURE;
+			}
+		}
+	}
 
 	/* Auto-start the miners here if we need to */
 	manager = tracker_miner_manager_new_full (TRUE, &error);
@@ -62,37 +179,59 @@ index_or_reindex_file (void)
 		g_printerr (_("Could not (re)index file, manager could not be created, %s"),
 		            error ? error->message : _("No error given"));
 		g_printerr ("\n");
-		g_clear_error (&error);
 		return EXIT_FAILURE;
 	}
 
+	main_loop = g_main_loop_new (NULL, 0);
+
 	for (p = filenames; *p; p++) {
-		GFile *file;
+		g_autoptr(GFile) file;
+		g_autoptr(TrackerIndexingStatus) status;
 
 		file = g_file_new_for_commandline_arg (*p);
-		tracker_miner_manager_index_file (manager, file, NULL, &error);
 
-		if (error) {
-			g_printerr ("%s: %s\n",
-			            _("Could not (re)index file"),
-			            error->message);
-			g_error_free (error);
-			return EXIT_FAILURE;
+		if (monitor_mode) {
+			status = tracker_miner_manager_index_file_for_process_async (manager, file, NULL, index_file_cb, main_loop);
+		} else {
+			status = tracker_miner_manager_index_file_async (manager, file, NULL, index_file_cb, main_loop);
 		}
 
-		g_print ("%s\n", _("(Re)indexing file was successful"));
-		g_object_unref (file);
+		if (no_wait) {
+			/* We may detect an error straight away, even if we don't wait. */
+			if (tracker_indexing_status_had_error (status)) {
+				print_indexing_status (file, status);
+				success = FALSE;
+			} else {
+				g_print ("Successfully enqueued %s for indexing.", *p);
+				success &= TRUE;
+			}
+		} else {
+			/* Run the main loop until the indexing completes, at which point
+			 * index_file_cb() will quit this loop.
+			 */
+			g_main_loop_run (main_loop);
+
+			print_indexing_status (file, status);
+
+			success &= !(tracker_indexing_status_had_error (status));
+		}
 	}
 
-	g_object_unref (manager);
+	if (monitor_mode) {
+		g_print (_("Press CTRL+C to exit and remove the files from the index."));
+		g_print ("\n");
 
-	return EXIT_SUCCESS;
-}
+		g_unix_signal_add (SIGINT, sigterm_cb, main_loop);
+		g_unix_signal_add (SIGTERM, sigterm_cb, main_loop);
 
-static int
-index_run (void)
-{
-	return index_or_reindex_file ();
+		g_main_loop_run (main_loop);
+	} else {
+		g_print (_("Files may not be monitored for changes. Use --monitor mode to "
+		           "avoid stale data being left in your Tracker index."));
+		g_print ("\n");
+	};
+
+	return success;
 }
 
 int
@@ -124,6 +263,8 @@ main (int argc, const char **argv)
 
 	if (!filenames || g_strv_length (filenames) < 1) {
 		failed = _("Please specify one or more locations to index.");
+	} else if (monitor_mode && no_wait) {
+		failed = _("The --monitor-mode and --no-wait options are incompatible");
 	} else {
 		failed = NULL;
 	}

sailfishos-mirror/localsearch

src/tracker/tracker-index.c
@@ -27,6 +27,7 @@
 #endif
 
 #include <glib.h>
+#include <glib-unix.h>
 #include <glib/gi18n.h>
 #include <gio/gio.h>
 #include <locale.h>
@@ -37,24 +38,165 @@
 #include "tracker-miner-manager.h"
 
 static gboolean file_arg;
+static gboolean monitor_mode;
+static gboolean no_wait;
 static gchar **filenames;
 
 static GOptionEntry entries[] = {
 	{ "file", 'f', 0, G_OPTION_ARG_NONE, &file_arg,
 	 N_("Does nothing, provided for compatibility with Tracker 2.0"),
 	 NULL },
+	{ "monitor", 'm', 0, G_OPTION_ARG_NONE, &monitor_mode,
+	  N_("Trigger indexing and wait for CTRL+C, removing the data again on exit."),
+	  NULL },
+	{ "no-wait", 'w', 0, G_OPTION_ARG_NONE, &no_wait,
+	  N_("Don't wait for processing to complete, exit immediately."),
+	  NULL },
 	{ G_OPTION_REMAINING, 0, 0, G_OPTION_ARG_FILENAME_ARRAY, &filenames,
 	  N_("FILE"),
 	  N_("FILE") },
 	{ NULL }
 };
 
-static gint
-index_or_reindex_file (void)
+GMainLoop *main_loop;
+
+static void
+print_indexing_status (GFile                 *root,
+                       TrackerIndexingStatus *status)
 {
-	TrackerMinerManager *manager;
+	g_autofree gchar *uri = g_file_get_uri (root);
+	g_autoptr(GList) error_list = NULL;
+
+	error_list = tracker_indexing_status_get_errors (status);
+
+	if (tracker_indexing_status_get_n_indexed_files (status) == 1) {
+		if (error_list) {
+			const gchar *message = error_list->data;
+			g_print ("%s\n", message);
+		} else {
+			/* TRANSLATORS: %s is a URI. */
+			g_print (_("File %s was processed successfully."), uri);
+			g_print ("\n");
+		}
+	} else {
+		GList *node;
+
+		if (tracker_indexing_status_get_completed (status)) {
+			if (error_list) {
+				/* TRANSLATORS: %s is a URI. */
+				g_print (_("Indexing of %s completed with errors."), uri);
+			} else {
+				/* TRANSLATORS: %s is a URI. */
+				g_print (_("Indexing of %s completed."), uri);
+			}
+			g_print ("\n");
+		}
+
+		if (error_list) {
+			for (node = error_list; node; node=node->next) {
+				const gchar *message = node->data;
+				g_print ("  * %s\n", message);
+			}
+		}
+
+		g_print (_("%i files were added to the index."),
+		         tracker_indexing_status_get_n_indexed_files (status));
+		g_print ("\n");
+	}
+
+	g_list_free_full (error_list, g_free);
+}
+
+static void
+index_file_cb (GObject      *source_object,
+               GAsyncResult *res,
+               gpointer      user_data)
+{
+	GMainLoop *loop = user_data;
 	GError *error = NULL;
+
+	tracker_miner_manager_index_file_finish (TRACKER_MINER_MANAGER (source_object), res, &error);
+
+	if (error) {
+		g_error ("Error starting indexing: %s", error->message);
+	}
+
+	g_main_loop_quit (loop);
+}
+
+static void
+index_file_for_process_cb (GObject      *source_object,
+               GAsyncResult *res,
+               gpointer      user_data)
+{
+	GMainLoop *loop = user_data;
+	GError *error = NULL;
+
+	tracker_miner_manager_index_file_for_process_finish (TRACKER_MINER_MANAGER (source_object), res, NULL);
+
+	if (error) {
+		g_error ("Error starting indexing: %s", error->message);
+	}
+
+	g_main_loop_quit (loop);
+}
+
+static gboolean
+g_file_is_directory (GFile *path)
+{
+	g_autoptr(GFileInfo) info = NULL;
+	g_autoptr(GError) error = NULL;
+
+	info = g_file_query_info (path,
+	                          G_FILE_ATTRIBUTE_STANDARD_TYPE,
+	                          G_FILE_QUERY_INFO_NONE,
+	                          NULL,
+	                          &error);
+
+	if (error) {
+		g_warning ("Error checking if %s is a directory: %s.",
+		           g_file_peek_path(path), error->message);
+		return FALSE;
+	}
+
+	return (g_file_info_get_file_type (info) == G_FILE_TYPE_DIRECTORY);
+}
+
+static gboolean
+sigterm_cb (gpointer user_data)
+{
+	g_message ("Received signal");
+
+	g_main_loop_quit (user_data);
+
+	return G_SOURCE_REMOVE;
+}
+
+static gint
+index_run (void)
+{
+	g_autoptr(TrackerMinerManager) manager;
+	g_autoptr(GMainLoop) main_loop;
+	g_autoptr(GError) error = NULL;
+	gboolean success = TRUE;
 	gchar **p;
+
+	/* Check we were only passed directories. IndexFileForProcess doesn't work
+	 * for files. */
+	if (monitor_mode) {
+		for (p = filenames; *p; p++) {
+			g_autoptr(GFile) path = NULL;
+
+			path = g_file_new_for_commandline_arg (*p);
+
+			if (!g_file_is_directory (path)) {
+				g_printerr (_("Could not index file %s: in `--monitor` mode, "
+				              "only directories can be indexed.\n"),
+				            g_file_peek_path (path));
+				return EXIT_FAILURE;
+			}
+		}
+	}
 
 	/* Auto-start the miners here if we need to */
 	manager = tracker_miner_manager_new_full (TRUE, &error);
@@ -62,37 +204,59 @@ index_or_reindex_file (void)
 		g_printerr (_("Could not (re)index file, manager could not be created, %s"),
 		            error ? error->message : _("No error given"));
 		g_printerr ("\n");
-		g_clear_error (&error);
 		return EXIT_FAILURE;
 	}
 
+	main_loop = g_main_loop_new (NULL, 0);
+
 	for (p = filenames; *p; p++) {
-		GFile *file;
+		g_autoptr(GFile) file;
+		g_autoptr(TrackerIndexingStatus) status;
 
 		file = g_file_new_for_commandline_arg (*p);
-		tracker_miner_manager_index_file (manager, file, NULL, &error);
 
-		if (error) {
-			g_printerr ("%s: %s\n",
-			            _("Could not (re)index file"),
-			            error->message);
-			g_error_free (error);
-			return EXIT_FAILURE;
+		if (monitor_mode) {
+			status = tracker_miner_manager_index_file_for_process_async (manager, file, NULL, index_file_for_process_cb, main_loop);
+		} else {
+			status = tracker_miner_manager_index_file_async (manager, file, NULL, index_file_cb, main_loop);
 		}
 
-		g_print ("%s\n", _("(Re)indexing file was successful"));
-		g_object_unref (file);
+		if (no_wait) {
+			/* We may detect an error straight away, even if we don't wait. */
+			if (tracker_indexing_status_had_error (status)) {
+				print_indexing_status (file, status);
+				success = FALSE;
+			} else {
+				g_print ("Successfully enqueued %s for indexing.", *p);
+				success &= TRUE;
+			}
+		} else {
+			/* Run the main loop until the indexing completes, at which point
+			 * index_file_cb() will quit this loop.
+			 */
+			g_main_loop_run (main_loop);
+
+			print_indexing_status (file, status);
+
+			success &= !(tracker_indexing_status_had_error (status));
+		}
 	}
 
-	g_object_unref (manager);
+	if (monitor_mode) {
+		g_print (_("Press CTRL+C to exit and remove the files from the index."));
+		g_print ("\n");
 
-	return EXIT_SUCCESS;
-}
+		g_unix_signal_add (SIGINT, sigterm_cb, main_loop);
+		g_unix_signal_add (SIGTERM, sigterm_cb, main_loop);
 
-static int
-index_run (void)
-{
-	return index_or_reindex_file ();
+		g_main_loop_run (main_loop);
+	} else {
+		g_print (_("Files may not be monitored for changes. Use --monitor mode to "
+		           "avoid stale data being left in your Tracker index."));
+		g_print ("\n");
+	};
+
+	return success ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
 int
@@ -124,6 +288,8 @@ main (int argc, const char **argv)
 
 	if (!filenames || g_strv_length (filenames) < 1) {
 		failed = _("Please specify one or more locations to index.");
+	} else if (monitor_mode && no_wait) {
+		failed = _("The --monitor-mode and --no-wait options are incompatible");
 	} else {
 		failed = NULL;
 	}