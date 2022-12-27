sailfishos-mirror/localsearch

commit 2425ed052355da382769206a7fd53c5848809cff

tracker-extract: Avoid the fluiddec gstreamer plugin
This is a module that attempts to decode MIDI and send it to the
soundcard. We don't want such access triggered from tracker-extract
(and it's actually caught by our seccomp sandbox). Avoid this plugin
altogether.

Fixes: https://gitlab.gnome.org/GNOME/tracker-miners/-/issues/101

src/tracker-extract/tracker-extract-gstreamer.c
@@ -1404,6 +1404,7 @@ tracker_extract_module_init (GError **error)
 	/* Lifted from totem-video-thumbnailer */
 	const gchar *blocklisted[] = {
 		"bcmdec",
+		"fluiddec",
 		"vaapi",
 		"video4linux2"
 	};

commit 7a9a43d9ed1deeb871529173cda043e562d8f763

tracker-extract: Drop the correct midi plugin name from the GST registry
Commit 2425ed05 attempted to drop fluidsynth GStreamer plugin which tries
to access hardware devices during metadata extraction and triggers the
seccomp jail. However the wrong name was pulled here, we should use the
name of the plugin itself, not the decoder element.

Fixes: 2425ed05 - tracker-extract: Avoid the fluiddec gstreamer plugin

Closes: https://gitlab.gnome.org/GNOME/tracker-miners/-/issues/249

src/tracker-extract/tracker-extract-gstreamer.c
@@ -1460,7 +1460,7 @@ tracker_extract_module_init (GError **error)
 	/* Lifted from totem-video-thumbnailer */
 	const gchar *blocklisted[] = {
 		"bcmdec",
-		"fluiddec",
+		"fluidsynthmidi",
 		"vaapi",
 		"video4linux2",
 		"nvcodec",