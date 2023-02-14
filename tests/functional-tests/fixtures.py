0d0e9afaaf859e2252d6e83d35915bbd129a19b3
Carlos Garnacho <carlosg@gnome.org>
2023-02-14 12:08:19 +0100

    tests: Handle gio changes
    At some point, gio started giving application/x-empty-file for
    0-size applications. This means that clearing all content from a
    text file like the fts-file-operations/02-empty-the-file functional
    test should result in the deletion of the file content from the
    content graph, since that mimetype no longer matches the
    tracker:Documents graph.
    
    Add this additional piece of handling in tests, the miner will
    be fixed along the following commits.

--- tests/functional-tests/fixtures.py
+++ tests/functional-tests/fixtures.py
@@ -264,7 +264,7 @@ class TrackerMinerFTSTest(TrackerMinerTest):
         text_escaped = Tracker.sparql_escape_string(text)
         path = pathlib.Path(self.path(self.testfile))
 
-        if path.exists():
+        if path.exists() and text != '':
             old_text_escaped = Tracker.sparql_escape_string(path.read_text())
             resource_id = self.tracker.get_content_resource_id(self.uri(self.testfile))
             with self.tracker.await_content_update(
@@ -275,7 +275,14 @@ class TrackerMinerFTSTest(TrackerMinerTest):
                 timeout=cfg.AWAIT_TIMEOUT,
             ):
                 path.write_text(text)
-        else:
+        elif path.exists() and text == '':
+            old_text_escaped = Tracker.sparql_escape_string(path.read_text())
+            resource_id = self.tracker.get_content_resource_id(self.uri(self.testfile))
+            with self.tracker.await_delete(DOCUMENTS_GRAPH,
+                                           resource_id,
+                                           timeout=cfg.AWAIT_TIMEOUT):
+                path.write_text(text)
+        elif text != '':
             url = self.uri(self.testfile)
             expected = f'a nfo:Document; nie:isStoredAs <{url}>; nie:plainTextContent "{text_escaped}"'
             with self.tracker.await_insert(
