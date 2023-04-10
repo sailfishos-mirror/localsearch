sailfishos-mirror/localsearch

commit 8d6508d9623ce6ee1b8ed7c9cd857ce5ee4ed2cc

tracker-miner-fs: Delegate TrackerEndpoint to a separate thread
The TrackerEndpointDBus set up here dispatches the incoming D-Bus
requests in the main context that is thread default at the time
of creating the endpoint. Currently this is the main thread, so
incoming SPARQL queries coming from external users might be delayed
by other miner activity (such as handling filesystem changes).

Move it to a separate dedicated thread, so that external SPARQL
queries are unimpeded by main thread activity. The underlying
TrackerSparqlConnection object handling both internal and external
queries can be used from any thread.

Closes: https://gitlab.gnome.org/GNOME/tracker/-/issues/382

src/miners/fs/tracker-main.c
@@ -104,6 +104,19 @@ static GOptionEntry entries[] = {
 	{ NULL }
 };
 
+typedef struct {
+	TrackerSparqlConnection *sparql_conn;
+	GDBusConnection *dbus_conn;
+	GMainLoop *main_loop;
+	GMutex mutex;
+	GCond cond;
+	GThread *thread;
+	gboolean initialized;
+	GError *error;
+} EndpointThreadData;
+
+static EndpointThreadData *endpoint_thread_data = NULL;
+
 static void
 log_option_values (TrackerConfig *config)
 {
@@ -808,37 +821,109 @@ get_fts_connection_flags (void)
 	return flags;
 }
 
-static gboolean
-setup_connection_and_endpoint (TrackerDomainOntology    *domain,
-                               GDBusConnection          *connection,
-                               TrackerSparqlConnection **sparql_conn,
-                               TrackerEndpointDBus     **endpoint,
-                               GError                  **error)
+gpointer
+endpoint_thread_func (gpointer user_data)
 {
+	EndpointThreadData *data = user_data;
+	TrackerEndpointDBus *endpoint;
+	GMainContext *main_context;
+
+	main_context = g_main_context_new ();
+	g_main_context_push_thread_default (main_context);
+
+	endpoint = tracker_endpoint_dbus_new (data->sparql_conn,
+	                                      data->dbus_conn,
+	                                      NULL,
+	                                      NULL,
+	                                      &data->error);
+
+	data->main_loop = g_main_loop_new (main_context, FALSE);
+	data->initialized = TRUE;
+
+	g_mutex_lock (&data->mutex);
+	g_cond_signal (&data->cond);
+	g_mutex_unlock (&data->mutex);
+
+	if (!data->error)
+		g_main_loop_run (data->main_loop);
+
+	g_main_context_pop_thread_default (main_context);
+	g_main_loop_unref (data->main_loop);
+	g_main_context_unref (main_context);
+	g_clear_object (&endpoint);
+
+	return NULL;
+}
+
+gboolean
+start_endpoint_thread (TrackerSparqlConnection  *conn,
+                       GDBusConnection          *dbus_conn,
+                       GError                  **error)
+{
+	g_assert (endpoint_thread_data == NULL);
+
+	endpoint_thread_data = g_new0 (EndpointThreadData, 1);
+	g_mutex_init (&endpoint_thread_data->mutex);
+	g_cond_init (&endpoint_thread_data->cond);
+	endpoint_thread_data->sparql_conn = conn;
+	endpoint_thread_data->dbus_conn = dbus_conn;
+
+	g_mutex_lock (&endpoint_thread_data->mutex);
+
+	endpoint_thread_data->thread =
+		g_thread_try_new ("SPARQL endpoint",
+		                  endpoint_thread_func,
+		                  endpoint_thread_data,
+		                  error);
+
+	if (!endpoint_thread_data->thread)
+		return FALSE;
+
+	while (!endpoint_thread_data->initialized) {
+		g_cond_wait (&endpoint_thread_data->cond,
+		             &endpoint_thread_data->mutex);
+	}
+
+	if (endpoint_thread_data->error) {
+		g_propagate_error (error, endpoint_thread_data->error);
+		g_thread_join (endpoint_thread_data->thread);
+		g_clear_pointer (&endpoint_thread_data, g_free);
+		return FALSE;
+	}
+
+	return TRUE;
+}
+
+void
+finish_endpoint_thread (void)
+{
+	g_assert (endpoint_thread_data != NULL);
+
+	g_main_loop_quit (endpoint_thread_data->main_loop);
+
+	g_thread_join (endpoint_thread_data->thread);
+
+	g_clear_pointer (&endpoint_thread_data, g_free);
+}
+
+static TrackerSparqlConnection *
+setup_connection (TrackerDomainOntology  *domain,
+                  GError                **error)
+{
+	TrackerSparqlConnection *sparql_conn;
 	GFile *store = NULL, *ontology;
 
 	if (!dry_run)
 		store = get_cache_dir (domain);
 	ontology = tracker_domain_ontology_get_ontology (domain);
-	*sparql_conn = tracker_sparql_connection_new (get_fts_connection_flags (),
-	                                              store,
-	                                              ontology,
-	                                              NULL,
-	                                              error);
+	sparql_conn = tracker_sparql_connection_new (get_fts_connection_flags (),
+	                                             store,
+	                                             ontology,
+	                                             NULL,
+	                                             error);
 	g_clear_object (&store);
 
-	if (!*sparql_conn)
-		return FALSE;
-
-	*endpoint = tracker_endpoint_dbus_new (*sparql_conn,
-	                                       connection,
-	                                       NULL,
-	                                       NULL,
-	                                       error);
-	if (!*endpoint)
-		return FALSE;
-
-	return TRUE;
+	return sparql_conn;
 }
 
 static void
@@ -962,7 +1047,6 @@ main (gint argc, gchar *argv[])
 	TrackerMinerProxy *proxy;
 	GDBusConnection *connection;
 	TrackerSparqlConnection *sparql_conn;
-	TrackerEndpointDBus *endpoint;
 	TrackerDomainOntology *domain_ontology;
 	GCancellable *cancellable;
 #if GLIB_CHECK_VERSION (2, 64, 0)
@@ -1059,13 +1143,18 @@ main (gint argc, gchar *argv[])
 		g_object_unref (store);
 	}
 
-	if (!setup_connection_and_endpoint (domain_ontology,
-	                                    connection,
-	                                    &sparql_conn,
-	                                    &endpoint,
-	                                    &error)) {
+	sparql_conn = setup_connection (domain_ontology, &error);
+	if (!sparql_conn) {
 
-		g_critical ("Could not create store/endpoint: %s",
+		g_critical ("Could not create store: %s",
+		            error->message);
+		g_error_free (error);
+
+		return EXIT_FAILURE;
+	}
+
+	if (!start_endpoint_thread (sparql_conn, connection, &error)) {
+		g_critical ("Could not set up SPARQL endpoint: %s",
 		            error->message);
 		g_error_free (error);
 
@@ -1189,6 +1278,8 @@ main (gint argc, gchar *argv[])
 		tracker_miner_files_set_need_mtime_check (TRACKER_MINER_FILES (miner_files), FALSE);
 		save_current_locale (domain_ontology);
 	}
+
+	finish_endpoint_thread ();
 
 	g_main_loop_unref (main_loop);
 	g_object_unref (config);

commit 8f30581b5eddd4e460da2e2e16a1a09205653a0f

tracker-miner-fs: Correctly wait for TrackerEndpoint thread initialization
Do things as per GCond documentation. Namely, initialize the wait condition
within the mutually exclusive area, as doing it outside may result in the
main thread going forward ahead of time.

This could result in situations that the endpoint thread is stuck on
initialization. Although it was mostly noticeable in CI, it looks like
a low-chance heisenbug that could make the endpoint not fully responsive.

This make flaky CI much less flaky.

Fixes: 8d6508d9623 ("tracker-miner-fs: Delegate TrackerEndpoint to a separate thread")

src/miners/fs/tracker-main.c
@@ -837,9 +837,9 @@ endpoint_thread_func (gpointer user_data)
 	                                      &data->error);
 
 	data->main_loop = g_main_loop_new (main_context, FALSE);
-	data->initialized = TRUE;
 
 	g_mutex_lock (&data->mutex);
+	data->initialized = TRUE;
 	g_cond_signal (&data->cond);
 	g_mutex_unlock (&data->mutex);
 
@@ -867,8 +867,6 @@ start_endpoint_thread (TrackerSparqlConnection  *conn,
 	endpoint_thread_data->sparql_conn = conn;
 	endpoint_thread_data->dbus_conn = dbus_conn;
 
-	g_mutex_lock (&endpoint_thread_data->mutex);
-
 	endpoint_thread_data->thread =
 		g_thread_try_new ("SPARQL endpoint",
 		                  endpoint_thread_func,
@@ -878,10 +876,14 @@ start_endpoint_thread (TrackerSparqlConnection  *conn,
 	if (!endpoint_thread_data->thread)
 		return FALSE;
 
+	g_mutex_lock (&endpoint_thread_data->mutex);
+
 	while (!endpoint_thread_data->initialized) {
 		g_cond_wait (&endpoint_thread_data->cond,
 		             &endpoint_thread_data->mutex);
 	}
+
+	g_mutex_unlock (&endpoint_thread_data->mutex);
 
 	if (endpoint_thread_data->error) {
 		g_propagate_error (error, endpoint_thread_data->error);