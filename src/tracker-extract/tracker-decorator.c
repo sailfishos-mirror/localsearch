sailfishos-mirror/localsearch

commit 1afc8bc05eedb7c29fca24d57550ee4abc64be9d

tracker-extract: Query remaining items through statement from a GResource
Use a generic query loaded from a GResource to get batches of unextracted
items.

src/tracker-extract/queries/get-items.rq
@@ -1,0 +1,48 @@
+# Inputs: graph1, graph2, graph3, graph4, graph5
+# Outputs: urn
+SELECT
+  ?urn
+  tracker:id(?urn)
+{
+  # We want to query graphs in a specific order
+  {
+    GRAPH ?g {
+      ?urn a nfo:FileDataObject
+    }
+    FILTER (STR (?g) = ~graph1)
+  }
+  UNION
+  {
+    GRAPH ?g {
+      ?urn a nfo:FileDataObject
+    }
+    FILTER (STR (?g) = ~graph2)
+  }
+  UNION
+  {
+    GRAPH ?g {
+      ?urn a nfo:FileDataObject
+    }
+    FILTER (STR (?g) = ~graph3)
+  }
+  UNION
+  {
+    GRAPH ?g {
+      ?urn a nfo:FileDataObject
+    }
+    FILTER (STR (?g) = ~graph4)
+  }
+  UNION
+  {
+    GRAPH ?g {
+      ?urn a nfo:FileDataObject
+    }
+    FILTER (STR (?g) = ~graph5)
+  }
+
+  FILTER (NOT EXISTS {
+    GRAPH tracker:FileSystem { ?urn tracker:extractorHash ?hash }
+  })
+}
+OFFSET ~offset
+LIMIT ~limit

src/tracker-extract/tracker-decorator.c
@@ -431,100 +431,6 @@ decorator_task_done (GObject      *object,
 	}
 }
 
-static gboolean
-append_graph_patterns (TrackerDecorator *decorator,
-                       GString          *query,
-                       gboolean          priority,
-                       gboolean          first)
-{
-	TrackerDecoratorPrivate *priv;
-	const gchar *graphs[] = {
-		"tracker:Audio",
-		"tracker:Pictures",
-		"tracker:Video",
-		"tracker:Software",
-		"tracker:Documents",
-	};
-	gint i;
-
-	priv = tracker_decorator_get_instance_private (decorator);
-
-	for (i = 0; i < G_N_ELEMENTS (graphs); i++) {
-		if (priority !=
-		    (priv->priority_graphs &&
-		     g_strv_contains ((const gchar * const *) priv->priority_graphs, graphs[i])))
-			continue;
-
-		if (!first)
-			g_string_append (query, "UNION ");
-
-		g_string_append_printf (query,
-		                        "{ GRAPH %s { ?urn a nfo:FileDataObject ; nfo:fileName [] } } ",
-		                        graphs[i]);
-		first = FALSE;
-	}
-
-	return first;
-}
-
-static gchar *
-create_query_string (TrackerDecorator  *decorator,
-                     gchar            **select_clauses)
-{
-	GString *query;
-	gboolean first;
-	gint i;
-
-	query = g_string_new ("SELECT ");
-
-	for (i = 0; select_clauses[i]; i++) {
-		g_string_append_printf (query, "%s ", select_clauses[i]);
-	}
-
-	g_string_append (query, "{ ");
-
-	/* Add priority graphs first, so they come up first in the query */
-	first = append_graph_patterns (decorator, query, TRUE, TRUE);
-	append_graph_patterns (decorator, query, FALSE, first);
-
-	g_string_append_printf (query,
-	                        "FILTER (NOT EXISTS {"
-	                        "  GRAPH tracker:FileSystem { ?urn tracker:extractorHash ?hash }"
-	                        "})"
-	                        "} OFFSET ~offset LIMIT %d",
-	                        QUERY_BATCH_SIZE);
-
-	return g_string_free (query, FALSE);
-}
-
-static TrackerSparqlStatement *
-create_prepared_statement (TrackerDecorator  *decorator,
-                           gchar            **select_clauses)
-{
-	TrackerDecoratorPrivate *priv;
-	TrackerSparqlConnection *sparql_conn;
-	TrackerSparqlStatement *statement;
-	GError *error = NULL;
-	gchar *query;
-
-	priv = tracker_decorator_get_instance_private (decorator);
-	query = create_query_string (decorator, select_clauses);
-
-	sparql_conn = tracker_miner_get_connection (TRACKER_MINER (decorator));
-	statement = tracker_sparql_connection_query_statement (sparql_conn,
-	                                                       query,
-	                                                       priv->cancellable,
-	                                                       &error);
-	g_free (query);
-
-	if (error) {
-		g_warning ("Could not create statement: %s", error->message);
-		g_error_free (error);
-	}
-
-	return statement;
-}
-
 static TrackerSparqlStatement *
 load_statement (TrackerDecorator *decorator,
                 const gchar      *query_filename)
@@ -542,24 +448,6 @@ load_statement (TrackerDecorator *decorator,
 	                                                                resource_path,
 	                                                                NULL,
 	                                                                NULL);
-}
-
-static TrackerSparqlStatement *
-ensure_remaining_items_query (TrackerDecorator *decorator)
-{
-	TrackerDecoratorPrivate *priv;
-	gchar *clauses[] = {
-		"?urn",
-		"tracker:id(?urn)",
-		NULL
-	};
-
-	priv = tracker_decorator_get_instance_private (decorator);
-
-	if (!priv->remaining_items_query)
-		priv->remaining_items_query = create_prepared_statement (decorator, clauses);
-
-	return priv->remaining_items_query;
 }
 
 static void
@@ -669,6 +557,75 @@ decorator_cache_items_cb (GObject      *object,
 }
 
 static void
+bind_graphs (TrackerDecorator *decorator,
+             int              *arg_counter,
+             gboolean          priority)
+{
+	TrackerDecoratorPrivate *priv;
+	TrackerSparqlConnection *conn;
+	TrackerNamespaceManager *namespaces;
+	const gchar *graphs[] = {
+		"tracker:Audio",
+		"tracker:Pictures",
+		"tracker:Video",
+		"tracker:Software",
+		"tracker:Documents",
+	};
+	guint i;
+
+	priv = tracker_decorator_get_instance_private (decorator);
+	conn = tracker_miner_get_connection (TRACKER_MINER (decorator));
+	namespaces = tracker_sparql_connection_get_namespace_manager (conn);
+
+	for (i = 0; i < G_N_ELEMENTS (graphs); i++) {
+		gboolean is_priority;
+		g_autofree gchar *arg = NULL, *expanded = NULL;
+
+		is_priority = priv->priority_graphs &&
+			g_strv_contains ((const gchar * const *) priv->priority_graphs, graphs[i]);
+
+		if (priority != is_priority)
+			continue;
+
+		arg = g_strdup_printf ("graph%d", (*arg_counter) + 1);
+		expanded = tracker_namespace_manager_expand_uri (namespaces, graphs[i]);
+		tracker_sparql_statement_bind_string (priv->remaining_items_query, arg, expanded);
+		(* arg_counter)++;
+	}
+}
+
+static void
+decorator_query_next_items (TrackerDecorator *decorator)
+{
+	TrackerDecoratorPrivate *priv;
+	gint offset = 0, n_graph = 0;
+
+	priv = tracker_decorator_get_instance_private (decorator);
+
+	if (priv->sparql_buffer)
+		offset += priv->sparql_buffer->len;
+	if (priv->commit_buffer)
+		offset += priv->commit_buffer->len;
+
+	if (!priv->remaining_items_query)
+		priv->remaining_items_query = load_statement (decorator, "get-items.rq");
+
+	/* Bind all graphs, in priority order */
+	bind_graphs (decorator, &n_graph, TRUE);
+	bind_graphs (decorator, &n_graph, FALSE);
+
+	tracker_sparql_statement_bind_int (priv->remaining_items_query,
+	                                   "offset", offset);
+	tracker_sparql_statement_bind_int (priv->remaining_items_query,
+	                                   "limit", QUERY_BATCH_SIZE);
+
+	tracker_sparql_statement_execute_async (priv->remaining_items_query,
+	                                        priv->cancellable,
+	                                        decorator_cache_items_cb,
+	                                        decorator);
+}
+
+static void
 decorator_cache_next_items (TrackerDecorator *decorator)
 {
 	TrackerDecoratorPrivate *priv;
@@ -686,21 +643,8 @@ decorator_cache_next_items (TrackerDecorator *decorator)
 		TRACKER_NOTE (DECORATOR, g_message ("[Decorator] Counting items which still need processing"));
 		decorator_count_remaining_items (decorator);
 	} else {
-		TrackerSparqlStatement *statement;
-		gint offset = 0;
-
-		if (priv->sparql_buffer)
-			offset += priv->sparql_buffer->len;
-		if (priv->commit_buffer)
-			offset += priv->commit_buffer->len;
-
 		TRACKER_NOTE (DECORATOR, g_message ("[Decorator] Querying items which still need processing"));
-		statement = ensure_remaining_items_query (decorator);
-		tracker_sparql_statement_bind_int (statement, "offset", offset);
-		tracker_sparql_statement_execute_async (statement,
-		                                        priv->cancellable,
-		                                        decorator_cache_items_cb,
-		                                        decorator);
+		decorator_query_next_items (decorator);
 	}
 }
 

src/tracker-extract/org.freedesktop.Tracker3.Extract.gresource.xml
@@ -3,6 +3,7 @@
   <gresource prefix="/org/freedesktop/Tracker3/Extract">
     <file>queries/delete-file.rq</file>
     <file>queries/get-item-count.rq</file>
+    <file>queries/get-items.rq</file>
     <file>queries/update-hash.rq</file>
   </gresource>
 </gresources>

commit 1ab5d5b704a4aab83acef1f5b85dc17f3d4cd201

tracker-extract: Regain performance at query to get unextracted items
In the tracker-extract query rework towards TrackerSparqlStatement and
static SPARQL queries built in GResources, the query to fetch unextracted
queries changed from a handcrafted version like:

  SELECT ... {
    { GRAPH ... { ?u a nfo:FileDataObject } } UNION
    { GRAPH ... { ?u a nfo:FileDataObject } } UNION
    ...
  }

That accessed each of the individual graphs in the fastest way possible,
ensuring the high-priority graphs get sorted first. This changed to a more
generic format in order to avoid in-place query string building:

  SELECT ... {
    { GRAPH ?g { ?u a nfo:FileDataObject } . FILTER (STR (?g) = ...) } UNION
    ...
  }

That basically results in, for each graph, querying all graphs in order
to filtering the results down to the desired one.

While this works, it multiplies the internal amount of churning and is
thus not great at performance. Since we query remaining elements in rather
small batches, the query time can get prohibitely high on a high enough
amount of indexed elementsn.

In order to improve on this, rework the query so that we get the best of
both worlds, in exchange of some parameter ugliness. The reworked query
works by querying everything in two blocks (for high and regular priority
graphs), for all graphs in each, but controlling the limit of each of
those subqueries:

  SELECT ... {
    {
      { SELECT ... { GRAPH ... { ?u a nfo:FileDataObject }  } LIMIT ~high } UNION
      ...
    } UNION {
      { SELECT ... { GRAPH ... { ?u a nfo:FileDataObject }  } LIMIT ~low } UNION
      ...
    }
  }

So high priority graphs get an unconstrained (-1) limit in the first block and
a no-op (0) limit in the second, and regular priority graphs get the opposite. This
results in direct graph access in the query without resolving ?g, no hand
crafting required, and as fast and efficient in memory as the former query.

The slight ugliness is that we have to specify, for each graph, the limits
for the high and regular priority blocks. Such is life for the sake of
performance.

Fixes: 1afc8bc05e

src/tracker-extract/queries/get-items.rq
@@ -1,43 +1,36 @@
-# Inputs: graph1, graph2, graph3, graph4, graph5
-# Outputs: urn
+# Inputs: documentsHigh, documentsLow, picturesHigh, picturesLow,
+#   audioHigh, audioLow, videoHigh, videoLow, softwareHigh, softwareLow
+# Outputs: urn, id
 SELECT
   ?urn
-  tracker:id(?urn)
+  tracker:id(?urn) ?AS ?id
 {
-  # We want to query graphs in a specific order
   {
-    GRAPH ?g {
-      ?urn a nfo:FileDataObject
+    # Data from high priority graphs
+    {
+      SELECT ?urn { GRAPH tracker:Documents { ?urn a nfo:FileDataObject } } LIMIT ~documentsHigh
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Pictures { ?urn a nfo:FileDataObject } } LIMIT ~picturesHigh
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Audio { ?urn a nfo:FileDataObject } } LIMIT ~audioHigh
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Video { ?urn a nfo:FileDataObject } } LIMIT ~videoHigh
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Software { ?urn a nfo:FileDataObject } } LIMIT ~softwareHigh
     }
-    FILTER (STR (?g) = ~graph1)
-  }
-  UNION
-  {
-    GRAPH ?g {
-      ?urn a nfo:FileDataObject
+  } UNION {
+    # Data from regular priority graphs
+    {
+      SELECT ?urn { GRAPH tracker:Documents { ?urn a nfo:FileDataObject } } LIMIT ~documentsLow
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Pictures { ?urn a nfo:FileDataObject } } LIMIT ~picturesLow
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Audio { ?urn a nfo:FileDataObject } } LIMIT ~audioLow
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Video { ?urn a nfo:FileDataObject } } LIMIT ~videoLow
+    } UNION {
+      SELECT ?urn { GRAPH tracker:Software { ?urn a nfo:FileDataObject } } LIMIT ~softwareLow
     }
-    FILTER (STR (?g) = ~graph2)
-  }
-  UNION
-  {
-    GRAPH ?g {
-      ?urn a nfo:FileDataObject
-    }
-    FILTER (STR (?g) = ~graph3)
-  }
-  UNION
-  {
-    GRAPH ?g {
-      ?urn a nfo:FileDataObject
-    }
-    FILTER (STR (?g) = ~graph4)
-  }
-  UNION
-  {
-    GRAPH ?g {
-      ?urn a nfo:FileDataObject
-    }
-    FILTER (STR (?g) = ~graph5)
   }
 
   FILTER (NOT EXISTS {

src/tracker-extract/tracker-decorator.c
@@ -566,40 +566,39 @@ decorator_cache_items_cb (GObject      *object,
 }
 
 static void
-bind_graphs (TrackerDecorator *decorator,
-             int              *arg_counter,
-             gboolean          priority)
+bind_graph_limits (TrackerDecorator *decorator,
+                   gboolean          priority)
 {
 	TrackerDecoratorPrivate *priv;
-	TrackerSparqlConnection *conn;
-	TrackerNamespaceManager *namespaces;
-	const gchar *graphs[] = {
-		"tracker:Audio",
-		"tracker:Pictures",
-		"tracker:Video",
-		"tracker:Software",
-		"tracker:Documents",
+	const gchar *graphs[][3] = {
+		{ "tracker:Audio", "audioHigh", "audioLow" },
+		{ "tracker:Pictures", "picturesHigh", "picturesLow" },
+		{ "tracker:Video", "videoHigh", "videoLow" },
+		{ "tracker:Software", "softwareHigh", "softwareLow" },
+		{ "tracker:Documents", "documentsHigh", "documentsLow" },
 	};
 	guint i;
 
 	priv = tracker_decorator_get_instance_private (decorator);
-	conn = tracker_miner_get_connection (TRACKER_MINER (decorator));
-	namespaces = tracker_sparql_connection_get_namespace_manager (conn);
 
 	for (i = 0; i < G_N_ELEMENTS (graphs); i++) {
+		const gchar *graph = graphs[i][0];
+		const gchar *high_limit = graphs[i][1];
+		const gchar *low_limit = graphs[i][2];
 		gboolean is_priority;
-		g_autofree gchar *arg = NULL, *expanded = NULL;
 
 		is_priority = priv->priority_graphs &&
-			g_strv_contains ((const gchar * const *) priv->priority_graphs, graphs[i]);
+			g_strv_contains ((const gchar * const *) priv->priority_graphs, graph);
 
-		if (priority != is_priority)
-			continue;
-
-		arg = g_strdup_printf ("graph%d", (*arg_counter) + 1);
-		expanded = tracker_namespace_manager_expand_uri (namespaces, graphs[i]);
-		tracker_sparql_statement_bind_string (priv->remaining_items_query, arg, expanded);
-		(* arg_counter)++;
+		/* Graphs with high priority get unbound high limit and 0 low limit,
+		 * graphs with regular priority get the opposite.
+		 */
+		tracker_sparql_statement_bind_int (priv->remaining_items_query,
+		                                   high_limit,
+		                                   is_priority ? -1 : 0);
+		tracker_sparql_statement_bind_int (priv->remaining_items_query,
+		                                   low_limit,
+		                                   is_priority ? 0 : -1);
 	}
 }
 
@@ -607,7 +606,7 @@ static void
 decorator_query_next_items (TrackerDecorator *decorator)
 {
 	TrackerDecoratorPrivate *priv;
-	gint offset = 0, n_graph = 0;
+	gint offset = 0;
 
 	priv = tracker_decorator_get_instance_private (decorator);
 
@@ -619,10 +618,7 @@ decorator_query_next_items (TrackerDecorator *decorator)
 	if (!priv->remaining_items_query)
 		priv->remaining_items_query = load_statement (decorator, "get-items.rq");
 
-	/* Bind all graphs, in priority order */
-	bind_graphs (decorator, &n_graph, TRUE);
-	bind_graphs (decorator, &n_graph, FALSE);
-
+	bind_graph_limits (decorator, TRUE);
 	tracker_sparql_statement_bind_int (priv->remaining_items_query,
 	                                   "offset", offset);
 	tracker_sparql_statement_bind_int (priv->remaining_items_query,