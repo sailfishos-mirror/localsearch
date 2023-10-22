e6fc5f4ca184040c61c82ca5cb95f6631b47e45c
Carlos Garnacho <carlosg@gnome.org>
2023-10-22 13:23:28 +0200

    ci: Look for matching ref in user remote for branch pipelines
    We have not enabled merge request pipelines yet on the tracker-miners
    repository, so the paths looking up for CI_MERGE_REQUEST_TARGET_BRANCH_NAME
    stand somewhat useless.
    
    In addition to looking for a matching ref in the official tracker git
    repository, also look up a possible matching tracker repository in the
    user namespace.

--- .gitlab-ci/checkout-tracker.sh
+++ .gitlab-ci/checkout-tracker.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+fetch() {
+  local remote=$1
+  local ref=$2
+
+  git fetch --quiet --depth=1 $remote $ref 2>/dev/null
+}
+
 tracker_target=
 
 mkdir subprojects
@@ -28,10 +35,17 @@ if [ "$CI_MERGE_REQUEST_TARGET_BRANCH_NAME" ]; then
 fi
 
 if [ -z "$tracker_target" ]; then
-  tracker_target=$(git branch -r -l origin/$CI_COMMIT_REF_NAME)
-  tracker_target=${tracker_target:-origin/master}
-  echo Using $tracker_target instead
+  ref_remote=${CI_PROJECT_URL//tracker-miners/tracker}
+  echo -n Looking for $CI_COMMIT_REF_NAME on remote...
+  if fetch $ref_remote $CI_COMMIT_REF_NAME; then
+    echo \ found
+    tracker_target=FETCH_HEAD
+  else
+    echo \ not found
+    tracker_target=$(git branch -r -l origin/$CI_COMMIT_REF_NAME)
+    tracker_target=${tracker_target:-origin/master}
+    echo Using $tracker_target instead
+  fi
 fi
 
 git checkout -q $tracker_target
-
