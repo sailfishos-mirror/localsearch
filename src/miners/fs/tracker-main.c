b4b6a3123867f72b494e79ffa3dad923d8e381a5
Carlos Garnacho <carlosg@gnome.org>
2024-02-23 09:35:19 +0100

    tracker-miner-fs: Minor refactor
    Don't duplicate function based on ifdef, and add helper functions
    to start/stop the memory release timeout.

--- src/miners/fs/tracker-main.c
+++ src/miners/fs/tracker-main.c
@@ -346,23 +346,15 @@ miner_start (TrackerMiner  *miner,
 	                                           miner);
 }
 
+static void
+release_heap_memory (void)
+{
 #ifdef HAVE_MALLOC_TRIM
-
-static void
-release_heap_memory (void)
-{
 	malloc_trim (0);
-}
-
 #else
-
-static void
-release_heap_memory (void)
-{
 	g_debug ("release_heap_memory(): Doing nothing as malloc_trim() is not available on this platform.");
-}
-
 #endif
+}
 
 static gboolean
 cleanup_cb (gpointer user_data)
@@ -372,6 +364,19 @@ cleanup_cb (gpointer user_data)
 	cleanup_id = 0;
 
 	return G_SOURCE_REMOVE;
+}
+
+static void
+start_cleanup_timeout (void)
+{
+	if (cleanup_id == 0)
+		cleanup_id = g_timeout_add_seconds (30, cleanup_cb, NULL);
+}
+
+static void
+stop_cleanup_timeout (void)
+{
+	g_clear_handle_id (&cleanup_id, g_source_remove);
 }
 
 #if GLIB_CHECK_VERSION (2, 64, 0)
@@ -388,10 +393,7 @@ on_low_memory (GMemoryMonitor            *monitor,
 static void
 miner_started_cb (TrackerMinerFS *fs)
 {
-	if (cleanup_id) {
-		g_source_remove (cleanup_id);
-		cleanup_id = 0;
-	}
+	stop_cleanup_timeout ();
 }
 
 static void
@@ -415,7 +417,7 @@ miner_finished_cb (TrackerMinerFS *fs,
 							 TRUE);
 	}
 
-	cleanup_id = g_timeout_add_seconds (30, cleanup_cb, NULL);
+	start_cleanup_timeout ();
 
 	/* We're not sticking around for file updates, so stop
 	 * the mainloop and exit.
