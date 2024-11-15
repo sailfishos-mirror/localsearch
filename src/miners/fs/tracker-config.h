86680d351419483d016f7f8ab41c50fcf9af6884
Carlos Garnacho <carlosg@gnome.org>
2024-11-16 00:32:50 +0100

    indexer: Clean up TrackerConfig object
    We don't need most of this anymore, just the thin wrappers to
    fetch recursive/single directories to be indexed.

--- src/miners/fs/tracker-config.h
+++ src/miners/fs/tracker-config.h
@@ -46,26 +46,8 @@ GType          tracker_config_get_type                             (void) G_GNUC
 
 TrackerConfig *tracker_config_new                                  (void);
 
-gint           tracker_config_get_initial_sleep                    (TrackerConfig *config);
-gboolean       tracker_config_get_enable_monitors                  (TrackerConfig *config);
-gint           tracker_config_get_throttle                         (TrackerConfig *config);
-gboolean       tracker_config_get_index_on_battery                 (TrackerConfig *config);
-gboolean       tracker_config_get_index_on_battery_first_time      (TrackerConfig *config);
-gboolean       tracker_config_get_index_removable_devices          (TrackerConfig *config);
-gboolean       tracker_config_get_index_optical_discs              (TrackerConfig *config);
-gboolean       tracker_config_get_index_mounted_directories        (TrackerConfig *config);
-gboolean       tracker_config_get_index_applications               (TrackerConfig *config);
-gint           tracker_config_get_low_disk_space_limit             (TrackerConfig *config);
 GSList *       tracker_config_get_index_recursive_directories      (TrackerConfig *config);
 GSList *       tracker_config_get_index_single_directories         (TrackerConfig *config);
-GSList *       tracker_config_get_ignored_directories              (TrackerConfig *config);
-GSList *       tracker_config_get_ignored_directories_with_content (TrackerConfig *config);
-GSList *       tracker_config_get_ignored_files                    (TrackerConfig *config);
-gint           tracker_config_get_crawling_interval                (TrackerConfig *config);
-gint           tracker_config_get_removable_days_threshold         (TrackerConfig *config);
-
-void           tracker_config_set_initial_sleep                    (TrackerConfig *config,
-                                                                    gint           value);
 
 G_END_DECLS
 
