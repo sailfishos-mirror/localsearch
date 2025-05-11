29d42839ee7db0500bbb53e99f8f153a00583277
Carlos Garnacho <carlosg@gnome.org>
2025-05-11 13:46:35 +0200

    cli: Simplify indexer status progress at "localsearch daemon --follow"
    Avoid use of TrackerMinerManager and use a single proxy to the LocalSearch
    indexer, and simplify output since we are no longer dealing with multiple
    daemons.
    
    Output has been made to reuse the same line in the terminal, unless it is
    being redirected somewhere else.

--- src/cli/tracker-daemon.c
+++ src/cli/tracker-daemon.c
@@ -22,10 +22,6 @@
 
 #include <errno.h>
 
-#ifdef __sun
-#include <procfs.h>
-#endif
-
 #include <glib.h>
 #include <glib-unix.h>
 #include <glib/gi18n.h>
@@ -38,19 +34,21 @@
 
 #include "tracker-process.h"
 #include "tracker-dbus.h"
-#include "tracker-miner-manager.h"
+#include "tracker-indexer-proxy.h"
 
 static GMainLoop *main_loop;
-static GHashTable *miners_progress;
-static GHashTable *miners_status;
-static gint longest_miner_name_length = 0;
-static gint paused_length = 0;
 
 static gboolean follow;
 static gboolean watch;
 
 static gboolean start;
 static gboolean terminate;
+
+static gboolean indexer_paused;
+
+static int indeterminate_pos = 0;
+
+#define INDETERMINATE_ROOM ((int) strlen ("100.0%") - 1)
 
 static GOptionEntry entries[] = {
 	/* Status */
@@ -104,186 +102,6 @@ initialize_signal_handler (void)
 	g_unix_signal_add (SIGINT, signal_handler, GINT_TO_POINTER (SIGINT));
 }
 
-static gboolean
-miner_get_details (TrackerMinerManager  *manager,
-                   const gchar          *miner,
-                   gchar               **status,
-                   gdouble              *progress,
-                   gint                 *remaining_time,
-                   GStrv                *pause_applications,
-                   GStrv                *pause_reasons)
-{
-	if ((status || progress || remaining_time) &&
-	    !tracker_miner_manager_get_status (manager,
-	                                       miner,
-	                                       status,
-	                                       progress,
-	                                       remaining_time)) {
-		g_printerr (_("Could not get status from miner: %s"), miner);
-		return FALSE;
-	}
-
-	tracker_miner_manager_is_paused (manager, miner,
-	                                 pause_applications,
-	                                 pause_reasons);
-
-	if (!(*pause_applications) || !(*pause_reasons)) {
-		/* unable to get pause details,
-		   already logged by tracker_miner_manager_is_paused */
-		return FALSE;
-	}
-
-	return TRUE;
-}
-
-static void
-miner_print_state (TrackerMinerManager *manager,
-                   const gchar         *miner_name,
-                   const gchar         *status,
-                   gdouble              progress,
-                   gint                 remaining_time,
-                   gboolean             is_running,
-                   gboolean             is_paused)
-{
-	const gchar *name;
-	time_t now;
-	gchar time_str[64];
-	size_t len;
-	struct tm *local_time;
-
-	now = time ((time_t *) NULL);
-	local_time = localtime (&now);
-	len = strftime (time_str,
-	                sizeof (time_str) - 1,
-	                "%d %b %Y, %H:%M:%S:",
-	                local_time);
-	time_str[len] = '\0';
-
-	name = tracker_miner_manager_get_display_name (manager, miner_name);
-
-	if (is_running) {
-		gchar *progress_str = NULL;
-		gchar *remaining_time_str = NULL;
-
-		if (progress >= 0.0 && progress < 1.0) {
-			progress_str = g_strdup_printf ("%3u%%", (guint)(progress * 100));
-		}
-
-		/* Progress > 0.01 here because we want to avoid any message
-		 * during crawling, as we don't have the remaining time in that
-		 * case and it would just print "unknown time left" */
-		if (progress > 0.01 &&
-		    progress < 1.0 &&
-		    remaining_time >= 0) {
-			/* 0 means that we couldn't properly compute the remaining
-			 * time. */
-			if (remaining_time > 0) {
-				gchar *seconds_str = tracker_seconds_to_string (remaining_time, TRUE);
-
-				/* Translators: %s is a time string */
-				remaining_time_str = g_strdup_printf (_("%s remaining"), seconds_str);
-				g_free (seconds_str);
-			} else {
-				remaining_time_str = g_strdup (_("unknown time left"));
-			}
-		}
-
-		g_print ("%s  %s  %-*.*s %s%-*.*s%s %s %s %s\n",
-		         time_str,
-		         progress_str ? progress_str : "✓   ",
-		         longest_miner_name_length,
-		         longest_miner_name_length,
-		         name,
-		         is_paused ? "(" : " ",
-		         paused_length,
-		         paused_length,
-		         is_paused ? _("PAUSED") : " ",
-		         is_paused ? ")" : " ",
-		         status ? "-" : "",
-		         status ? _(status) : "",
-		         remaining_time_str ? remaining_time_str : "");
-
-		g_free (progress_str);
-		g_free (remaining_time_str);
-	} else {
-		g_print ("%s  ✗     %-*.*s  %-*.*s  - %s\n",
-		         time_str,
-		         longest_miner_name_length,
-		         longest_miner_name_length,
-		         name,
-		         paused_length,
-		         paused_length,
-		         " ",
-		         _("Not running or is a disabled plugin"));
-	}
-}
-
-static void
-manager_miner_progress_cb (TrackerMinerManager *manager,
-                           const gchar         *miner_name,
-                           const gchar         *status,
-                           gdouble              progress,
-                           gint                 remaining_time)
-{
-	GValue *gvalue;
-
-	gvalue = g_slice_new0 (GValue);
-
-	g_value_init (gvalue, G_TYPE_DOUBLE);
-	g_value_set_double (gvalue, progress);
-
-	miner_print_state (manager, miner_name, status, progress, remaining_time, TRUE, FALSE);
-
-	g_hash_table_replace (miners_status,
-	                      g_strdup (miner_name),
-	                      g_strdup (status));
-	g_hash_table_replace (miners_progress,
-	                      g_strdup (miner_name),
-	                      gvalue);
-}
-
-static void
-manager_miner_paused_cb (TrackerMinerManager *manager,
-                         const gchar         *miner_name)
-{
-	GValue *gvalue;
-
-	gvalue = g_hash_table_lookup (miners_progress, miner_name);
-
-	miner_print_state (manager, miner_name,
-	                   g_hash_table_lookup (miners_status, miner_name),
-	                   gvalue ? g_value_get_double (gvalue) : 0.0,
-	                   -1,
-	                   TRUE,
-	                   TRUE);
-}
-
-static void
-manager_miner_resumed_cb (TrackerMinerManager *manager,
-                          const gchar         *miner_name)
-{
-	GValue *gvalue;
-
-	gvalue = g_hash_table_lookup (miners_progress, miner_name);
-
-	miner_print_state (manager, miner_name,
-	                   g_hash_table_lookup (miners_status, miner_name),
-	                   gvalue ? g_value_get_double (gvalue) : 0.0,
-	                   0,
-	                   TRUE,
-	                   FALSE);
-}
-
-static void
-miners_progress_destroy_notify (gpointer data)
-{
-	GValue *value;
-
-	value = data;
-	g_value_unset (value);
-	g_slice_free (GValue, value);
-}
-
 static void
 notifier_events_cb (TrackerNotifier         *notifier,
 		    const gchar             *service,
@@ -302,14 +120,124 @@ notifier_events_cb (TrackerNotifier         *notifier,
 	}
 }
 
+static void
+maybe_reset_line (void)
+{
+	if (tracker_term_is_tty ()) {
+		g_print ("\033[2K");
+		g_print ("\r");
+	}
+}
+
+static void
+print_indexer_status (TrackerIndexerMiner *indexer_proxy)
+{
+	g_auto (GStrv) pause_apps = NULL, pause_reasons = NULL;
+	g_autofree char *status = NULL;
+	double progress;
+
+	if (!tracker_indexer_miner_call_get_status_sync (indexer_proxy,
+	                                                 &status,
+	                                                 NULL, NULL))
+		return;
+
+	if (!tracker_indexer_miner_call_get_progress_sync (indexer_proxy,
+	                                                   &progress,
+	                                                   NULL, NULL))
+		return;
+
+	maybe_reset_line ();
+
+	if (progress > 0.0) {
+		g_print ("[%5.1f%%]", progress * 100);
+	} else {
+		g_print ("[");
+		if (indeterminate_pos > 0)
+			g_print ("%*c", indeterminate_pos, ' ');
+		g_print ("=");
+		if (INDETERMINATE_ROOM - indeterminate_pos > 0)
+			g_print ("%*c", INDETERMINATE_ROOM - indeterminate_pos, ' ');
+		g_print ("]");
+
+		indeterminate_pos++;
+		if (indeterminate_pos > INDETERMINATE_ROOM)
+			indeterminate_pos = 0;
+	}
+
+	g_print (" %s", status);
+
+	if (!tracker_term_is_tty ())
+		g_print ("\n");
+}
+
+static void
+indexer_progress_cb (TrackerIndexerMiner *indexer_proxy,
+                     const char          *status,
+                     double               progress,
+                     int                  remaining_time)
+{
+	if (!indexer_paused)
+		print_indexer_status (indexer_proxy);
+}
+
+static void
+indexer_paused_cb (TrackerIndexerMiner *indexer_proxy)
+{
+	maybe_reset_line ();
+	g_print ("%s", _("Indexer is paused"));
+	indexer_paused = TRUE;
+}
+
+static void
+indexer_resumed_cb (TrackerIndexerMiner *indexer_proxy)
+{
+	indexer_paused = FALSE;
+}
+
+static int
+daemon_status (void)
+{
+	g_autoptr (TrackerIndexerMiner) indexer_proxy = NULL;
+
+	indexer_proxy =
+		tracker_indexer_miner_proxy_new_for_bus_sync (G_BUS_TYPE_SESSION,
+		                                              G_DBUS_PROXY_FLAGS_DO_NOT_AUTO_START,
+		                                              "org.freedesktop.LocalSearch3",
+		                                              "/org/freedesktop/Tracker3/Miner/Files",
+		                                              NULL, NULL);
+	if (!indexer_proxy)
+		return EXIT_FAILURE;
+
+	print_indexer_status (indexer_proxy);
+
+	if (follow) {
+		g_signal_connect (indexer_proxy, "progress",
+		                  G_CALLBACK (indexer_progress_cb), NULL);
+		g_signal_connect (indexer_proxy, "paused",
+		                  G_CALLBACK (indexer_paused_cb), NULL);
+		g_signal_connect (indexer_proxy, "resumed",
+		                  G_CALLBACK (indexer_resumed_cb), NULL);
+
+		initialize_signal_handler ();
+
+		main_loop = g_main_loop_new (NULL, FALSE);
+		g_main_loop_run (main_loop);
+		g_main_loop_unref (main_loop);
+
+		if (tracker_term_is_tty ()) {
+			/* Print the status line a last time, papering over the ^C */
+			print_indexer_status (indexer_proxy);
+			g_print ("\n");
+		}
+	}
+
+	return EXIT_SUCCESS;
+}
+
 static gint
 daemon_run (void)
 {
-	TrackerMinerManager *manager;
 	g_autoptr (GError) error = NULL;
-	GSList *miners_available;
-	GSList *miners_running;
-	GSList *l;
 
 	if (watch) {
 		TrackerSparqlConnection *sparql_connection;
@@ -367,123 +295,7 @@ daemon_run (void)
 		return EXIT_SUCCESS;
 	}
 
-	/* Don't auto-start the miners here */
-	manager = tracker_miner_manager_new_full (FALSE, &error);
-	if (!manager) {
-		g_printerr (_("Could not get status, manager could not be created, %s"),
-		            error ? error->message : _("No error given"));
-		g_printerr ("\n");
-		g_clear_error (&error);
-		return EXIT_FAILURE;
-	}
-
-	miners_available = tracker_miner_manager_get_available (manager);
-	miners_running = tracker_miner_manager_get_running (manager);
-
-	/* Work out lengths for output spacing */
-	paused_length = strlen (_("PAUSED"));
-
-	for (l = miners_available; l; l = l->next) {
-		const gchar *name;
-
-		name = tracker_miner_manager_get_display_name (manager, l->data);
-		longest_miner_name_length = MAX (longest_miner_name_length, strlen (name));
-	}
-
-	/* Display states */
-	g_print ("%s:\n", _("Miners"));
-
-	for (l = miners_available; l; l = l->next) {
-		const gchar *name;
-		gboolean is_running;
-
-		name = tracker_miner_manager_get_display_name (manager, l->data);
-		if (!name) {
-			g_critical (_("Could not get display name for miner “%s”"),
-			            (const gchar*) l->data);
-			continue;
-		}
-
-		is_running = tracker_string_in_gslist (l->data, miners_running);
-
-		if (is_running) {
-			GStrv pause_applications, pause_reasons;
-			gchar *status = NULL;
-			gdouble progress;
-			gint remaining_time;
-			gboolean is_paused;
-
-			if (!miner_get_details (manager,
-			                        l->data,
-			                        &status,
-			                        &progress,
-			                        &remaining_time,
-			                        &pause_applications,
-			                        &pause_reasons)) {
-				continue;
-			}
-
-			is_paused = *pause_applications || *pause_reasons;
-
-			miner_print_state (manager,
-			                   l->data,
-			                   status,
-			                   progress,
-			                   remaining_time,
-			                   TRUE,
-			                   is_paused);
-
-			g_strfreev (pause_applications);
-			g_strfreev (pause_reasons);
-			g_free (status);
-		} else {
-			miner_print_state (manager, l->data, NULL, 0.0, -1, FALSE, FALSE);
-		}
-	}
-
-	g_slist_foreach (miners_available, (GFunc) g_free, NULL);
-	g_slist_free (miners_available);
-
-	g_slist_foreach (miners_running, (GFunc) g_free, NULL);
-	g_slist_free (miners_running);
-
-	if (follow) {
-		g_print ("%s\n", _("Press Ctrl+C to stop"));
-
-		g_signal_connect (manager, "miner-progress",
-		                  G_CALLBACK (manager_miner_progress_cb), NULL);
-		g_signal_connect (manager, "miner-paused",
-		                  G_CALLBACK (manager_miner_paused_cb), NULL);
-		g_signal_connect (manager, "miner-resumed",
-		                  G_CALLBACK (manager_miner_resumed_cb), NULL);
-
-		initialize_signal_handler ();
-
-		miners_progress = g_hash_table_new_full (g_str_hash,
-		                                         g_str_equal,
-		                                         (GDestroyNotify) g_free,
-		                                         (GDestroyNotify) miners_progress_destroy_notify);
-		miners_status = g_hash_table_new_full (g_str_hash,
-		                                       g_str_equal,
-		                                       (GDestroyNotify) g_free,
-		                                       (GDestroyNotify) g_free);
-
-		main_loop = g_main_loop_new (NULL, FALSE);
-		g_main_loop_run (main_loop);
-		g_main_loop_unref (main_loop);
-
-		/* Carriage return, so we paper over the ^C */
-		g_print ("\r");
-
-		g_hash_table_unref (miners_progress);
-		g_hash_table_unref (miners_status);
-	}
-
-	if (manager) {
-		g_object_unref (manager);
-	}
-
-	return EXIT_SUCCESS;
+	return daemon_status ();
 }
 
 int
