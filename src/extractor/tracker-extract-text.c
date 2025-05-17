sailfishos-mirror/localsearch

commit 3bef7fb0bf963b140f76942a7b131ffa4929c81e

extractor: Make error check more appealing to Coverity
So that it doesn't think the file content can be leaked.

CID: #375239

src/extractor/tracker-extract-text.c
@@ -93,7 +93,7 @@ tracker_extract_get_metadata (TrackerExtractInfo  *info,
 	                            tracker_extract_info_get_max_text (info),
 	                            &inner_error);
 
-	if (inner_error != NULL) {
+	if (!content) {
 		/* An error occurred, perhaps the file was deleted. */
 		g_propagate_prefixed_error (error, inner_error, "Could not open:");
 		return FALSE;

commit b36b0625e20b9a8df3ca26c2d6a77be5ae95cdcd

extractor: Do not try to propagate a NULL error on empty text files
This error check was changed at 3bef7fb0bf96 in order to appease
Coverity, but there are situations where a NULL content can be gotten
with no error associated, e.g. files with an indeterminate encoding.

Revert the check, and rely on g_auto to make everything more reassuring
to Coverity.

Fixes: 3bef7fb0b ("extractor: Make error check more appealing to Coverity")

src/extractor/tracker-extract-text.c
@@ -80,29 +80,28 @@ G_MODULE_EXPORT gboolean
 tracker_extract_get_metadata (TrackerExtractInfo  *info,
                               GError             **error)
 {
-	TrackerResource *metadata;
-	gchar *content = NULL, *resource_uri;
+	g_autoptr (TrackerResource) metadata = NULL;
+	g_autofree char *content = NULL, *resource_uri = NULL;
 	GError *inner_error = NULL;
 
 	resource_uri = tracker_extract_info_get_content_id (info, NULL);
 	metadata = tracker_resource_new (resource_uri);
 	tracker_resource_add_uri (metadata, "rdf:type", "nfo:PlainTextDocument");
-	g_free (resource_uri);
 
 	content = get_file_content (tracker_extract_info_get_file (info),
 	                            tracker_extract_info_get_max_text (info),
 	                            &inner_error);
 
-	if (!content) {
+	if (inner_error) {
 		/* An error occurred, perhaps the file was deleted. */
 		g_propagate_prefixed_error (error, inner_error, "Could not open:");
 		return FALSE;
 	}
 
-	tracker_resource_set_string (metadata, "nie:plainTextContent", content);
+	if (content)
+		tracker_resource_set_string (metadata, "nie:plainTextContent", content);
+
 	tracker_extract_info_set_resource (info, metadata);
-	g_object_unref (metadata);
-	g_free (content);
 
 	return TRUE;
 }