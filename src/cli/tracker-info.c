60689646eb87a48c21687d9fd9434481e9b8298b
Carlos Garnacho <carlosg@gnome.org>
2025-05-27 12:45:01 +0200

    cli: Update path to access localsearch-3 while in build dir
    This did not update to the folder shuffling.

--- src/cli/tracker-info.c
+++ src/cli/tracker-info.c
@@ -308,8 +308,8 @@ output_eligible_status_for_file (gchar   *path,
 	g_autofree char *tracker_miner_fs_path = NULL;
 
 	if (inside_build_tree) {
-		/* Developer convienence - use uninstalled version if running from build tree */
-		tracker_miner_fs_path = g_build_filename (BUILDROOT, "src", "miners", "fs", "localsearch-3", NULL);
+		/* Developer convenience - use uninstalled version if running from build tree */
+		tracker_miner_fs_path = g_build_filename (BUILDROOT, "src", "indexer", "localsearch-3", NULL);
 	} else {
 		tracker_miner_fs_path = g_build_filename (LIBEXECDIR, "localsearch-3", NULL);
 	}
