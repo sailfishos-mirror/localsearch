sailfishos-mirror/localsearch

commit c1411c4bf968483c8430318eb5414b9633a498b8

indexer: Notify corruption through our own file
Commit c454af95cce snuck the handling of constraint errors as
database corruptions in an insufficient manner. This would indeed
make the indexer exit swiftly instead of making a scene with a
barrage of warnings, but the untouched second half of this handling
is what happens on the next indexer restart, which was so far
nothing, the indexer would restart again, and probably find the
same constraint error, since the database is not corrupted in a
way that it will fail on construction, just in a way LocalSearch
knows it lost consistency.

To fix this, make LocalSearch itself track database corruption
at runtime with an ancillary hidden file in the cache dir, and
check for it existing at startup like we do with natural CORRUPTED
errors happening from the database.

We may want to handle other error types the same way in the future,
so an additional ancillary file can fit the bill for them all.

src/indexer/tracker-application.c
@@ -44,6 +44,8 @@
 	"\n" \
 	"  http://www.gnu.org/licenses/gpl.txt\n"
 
+#define CORRUPT_FILE_NAME ".localsearch.corrupted"
+
 static GOptionEntry entries[] = {
 	{ "no-daemon", 'n', 0,
 	  G_OPTION_ARG_NONE, NULL,
@@ -254,8 +256,9 @@ setup_connection (TrackerApplication  *app,
                   GError             **error)
 {
 	TrackerSparqlConnection *sparql_conn;
-	g_autoptr (GFile) store = NULL, ontology = NULL;
+	g_autoptr (GFile) store = NULL, ontology = NULL, corrupt = NULL;
 	g_autoptr (GError) internal_error = NULL;
+	gboolean is_corrupted;
 	TrackerSparqlConnectionFlags flags =
 		TRACKER_SPARQL_CONNECTION_FLAGS_FTS_ENABLE_STEMMER |
 		TRACKER_SPARQL_CONNECTION_FLAGS_FTS_ENABLE_UNACCENT;
@@ -264,33 +267,41 @@ setup_connection (TrackerApplication  *app,
 		store = get_cache_dir ();
 	ontology = tracker_sparql_get_ontology_nepomuk ();
 
-	sparql_conn = tracker_sparql_connection_new (flags,
-	                                             store,
-	                                             ontology,
-	                                             NULL,
-	                                             &internal_error);
+	corrupt = g_file_get_child (store, CORRUPT_FILE_NAME);
+	is_corrupted = g_file_query_exists (corrupt, NULL);
 
-	if (store && g_error_matches (internal_error, TRACKER_SPARQL_ERROR, TRACKER_SPARQL_ERROR_CORRUPT)) {
-		g_autoptr (GFile) backup_location = NULL, parent = NULL;
-		g_autofree gchar *filename = NULL;
+	if (!store || !is_corrupted) {
+		sparql_conn = tracker_sparql_connection_new (flags,
+							     store,
+							     ontology,
+							     NULL,
+							     &internal_error);
+	}
 
-		/* Move the database directory away, for possible forensics */
-		parent = g_file_get_parent (store);
-		filename = g_strdup_printf ("files.%" G_GINT64_FORMAT, g_get_monotonic_time ());
-		backup_location = g_file_get_child (parent, filename);
+	if (store) {
+		if (is_corrupted ||
+		    g_error_matches (internal_error, TRACKER_SPARQL_ERROR, TRACKER_SPARQL_ERROR_CORRUPT)) {
+			g_autoptr (GFile) backup_location = NULL, parent = NULL;
+			g_autofree gchar *filename = NULL;
 
-		if (g_file_move (store, backup_location,
-		                 G_FILE_COPY_NONE,
-		                 NULL, NULL, NULL, NULL)) {
-			g_autofree gchar *path = NULL;
+			/* Move the database directory away, for possible forensics */
+			parent = g_file_get_parent (store);
+			filename = g_strdup_printf ("files.%" G_GINT64_FORMAT, g_get_monotonic_time ());
+			backup_location = g_file_get_child (parent, filename);
 
-			path = g_file_get_path (backup_location);
-			g_message ("Database is corrupt, it is now backed up at %s. Reindexing from scratch", path);
-			sparql_conn = tracker_sparql_connection_new (flags,
-			                                             store,
-			                                             ontology,
-			                                             NULL,
-			                                             &internal_error);
+			if (g_file_move (store, backup_location,
+					 G_FILE_COPY_NONE,
+					 NULL, NULL, NULL, NULL)) {
+				g_autofree gchar *path = NULL;
+
+				path = g_file_get_path (backup_location);
+				g_message ("Database is corrupt, it is now backed up at %s. Reindexing from scratch", path);
+				sparql_conn = tracker_sparql_connection_new (flags,
+									     store,
+									     ontology,
+									     NULL,
+									     &internal_error);
+			}
 		}
 	}
 
@@ -378,6 +389,14 @@ indexer_status_cb (TrackerApplication *app)
 static void
 indexer_corrupt_cb (TrackerApplication *app)
 {
+	g_autoptr (GFile) cache_dir = NULL, corrupt_file = NULL;
+	g_autoptr (GError) error = NULL;
+
+	cache_dir = get_cache_dir ();
+	corrupt_file = g_file_get_child (cache_dir, CORRUPT_FILE_NAME);
+	if (!g_file_set_contents (g_file_peek_path (corrupt_file), "", -1, &error))
+		g_warning ("Could not mark database as corrupt: %s", error->message);
+
 	g_warning ("Database corruption detected, bailing out");
 	app->got_error = TRUE;
 	g_application_quit (G_APPLICATION (app));

commit 45f581354b31e6a8f35eccda669de080f9effc3f

indexer: Initialize variable
Fixes this compiler warning:

In function ‘setup_connection’,
    inlined from ‘tracker_application_initable_init’ at ../src/indexer/tracker-application.c:737:35:
../src/indexer/tracker-application.c:307:20: warning: ‘sparql_conn’ may be used uninitialized [-Wmaybe-uninitialized]
  307 |                 if (sparql_conn)
      |                    ^
../src/indexer/tracker-application.c: In function ‘tracker_application_initable_init’:
../src/indexer/tracker-application.c:258:34: note: ‘sparql_conn’ was declared here
  258 |         TrackerSparqlConnection *sparql_conn;
      |                                  ^~~~~~~~~~~

Fixes: c1411c4bf ("indexer: Notify corruption through our own file")

src/indexer/tracker-application.c
@@ -255,7 +255,7 @@ static TrackerSparqlConnection *
 setup_connection (TrackerApplication  *app,
                   GError             **error)
 {
-	TrackerSparqlConnection *sparql_conn;
+	TrackerSparqlConnection *sparql_conn = NULL;
 	g_autoptr (GFile) store = NULL, ontology = NULL, corrupt = NULL;
 	g_autoptr (GError) internal_error = NULL;
 	gboolean is_corrupted;