sailfishos-mirror/localsearch

commit 5c22f1e2c58a3d88af01fecfbe82fe519d540691

indexer: Avoid grace timeout in restarting extractor
Back in the day, the extractor was a D-Bus service in the session
bus, and it was possible to restart it out of band, and still have it
talk with the indexer, and this was useful for debugging purposes.

Nowadays, the extractor is always a subprocess of the indexer, and
debugging should happen as a whole through child tracing. This grace
period does not do much anymore, so make it so the restart is immediate.

src/indexer/tracker-extract-watchdog.c
@@ -373,6 +373,7 @@ wait_check_async_cb (GObject      *object,
 {
 	TrackerExtractWatchdog *watchdog = user_data;
 	g_autoptr (GError) error = NULL;
+	gboolean success;
 
 	if (!g_subprocess_wait_check_finish (watchdog->extract_process,
 	                                     res, &error)) {
@@ -380,12 +381,12 @@ wait_check_async_cb (GObject      *object,
 			return;
 
 		g_warning ("Extractor subprocess died unexpectedly: %s", error->message);
+		clear_process_state (watchdog);
 		g_signal_emit (watchdog, signals[LOST], 0);
 	} else {
 		g_signal_emit (watchdog, signals[STATUS], 0, "Idle", 1.0, 0);
+		clear_process_state (watchdog);
 	}
-
-	clear_process_state (watchdog);
 }
 
 static GStrv

src/indexer/tracker-indexer.c
@@ -99,7 +99,6 @@ struct _TrackerIndexer {
 	guint flushing : 1;         /* TRUE if flushing SPARQL */
 
 	guint status_idle_id;
-	guint grace_period_timeout_id;
 	guint resume_after_disk_full_id;
 	guint item_queues_handler_id;
 };
@@ -522,16 +521,6 @@ check_unextracted (TrackerIndexer *indexer)
 	tracker_extract_watchdog_ensure_started (indexer->extract_watchdog);
 }
 
-static gboolean
-extractor_lost_timeout_cb (gpointer user_data)
-{
-	TrackerIndexer *indexer = user_data;
-
-	check_unextracted (indexer);
-	indexer->grace_period_timeout_id = 0;
-	return G_SOURCE_REMOVE;
-}
-
 static void
 set_active (TrackerIndexer *indexer,
             gboolean        active)
@@ -555,12 +544,7 @@ on_extractor_lost (TrackerExtractWatchdog *watchdog,
                    TrackerIndexer         *indexer)
 {
 	g_debug ("tracker-extract vanished, maybe restarting.");
-
-	/* Give a period of grace before restarting, so we allow replacing
-	 * from eg. a terminal.
-	 */
-	indexer->grace_period_timeout_id =
-		g_timeout_add_seconds (1, extractor_lost_timeout_cb, indexer);
+	check_unextracted (indexer);
 }
 
 static void
@@ -696,7 +680,6 @@ fs_finalize (GObject *object)
 
 	g_clear_pointer (&indexer->urn_lru, tracker_lru_free);
 	g_clear_handle_id (&indexer->item_queues_handler_id, g_source_remove);
-	g_clear_handle_id (&indexer->grace_period_timeout_id, g_source_remove);
 	g_clear_handle_id (&indexer->resume_after_disk_full_id, g_source_remove);
 
 	if (indexer->file_notifier)

commit 83ff776f5a40bf6f967a47732bd8475413b5d09a

indexer: Drop unused variable
This was unused from the start.

Fixes: 5c22f1e2c ("indexer: Avoid grace timeout in restarting extractor")

src/indexer/tracker-extract-watchdog.c
@@ -373,7 +373,6 @@ wait_check_async_cb (GObject      *object,
 {
 	TrackerExtractWatchdog *watchdog = user_data;
 	g_autoptr (GError) error = NULL;
-	gboolean success;
 
 	if (!g_subprocess_wait_check_finish (watchdog->extract_process,
 	                                     res, &error)) {