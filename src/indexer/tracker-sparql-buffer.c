sailfishos-mirror/localsearch

commit 162034c5baa7a8089b39b3d9846d1221f1ff70c6

indexer: Cleanup indirect resources
Schedule a cleanup of indirect audio resources (albums, discs, artists)
that might be left dangling after files are deleted from the database.
So far, this cleanup happens right after any operation that might have
deleted any file.

Closes: https://gitlab.gnome.org/GNOME/localsearch/-/work_items/441

src/indexer/tracker-sparql-buffer.c
@@ -57,6 +57,9 @@ struct _TrackerSparqlBuffer
 	TrackerSparqlStatement *delete_content;
 	TrackerSparqlStatement *move_file;
 	TrackerSparqlStatement *move_content;
+	TrackerSparqlStatement *cleanup_audio_album_discs;
+	TrackerSparqlStatement *cleanup_audio_albums;
+	TrackerSparqlStatement *cleanup_audio_artists;
 
 	GFile *root;
 };
@@ -104,6 +107,9 @@ tracker_sparql_buffer_finalize (GObject *object)
 	g_object_unref (sparql_buffer->move_content);
 	g_object_unref (sparql_buffer->connection);
 	g_clear_object (&sparql_buffer->root);
+	g_clear_object (&sparql_buffer->cleanup_audio_album_discs);
+	g_clear_object (&sparql_buffer->cleanup_audio_albums);
+	g_clear_object (&sparql_buffer->cleanup_audio_artists);
 
 	G_OBJECT_CLASS (tracker_sparql_buffer_parent_class)->finalize (object);
 }
@@ -147,6 +153,12 @@ tracker_sparql_buffer_constructed (GObject *object)
 		tracker_load_statement (sparql_buffer->connection, "move-file.rq", NULL);
 	sparql_buffer->move_content =
 		tracker_load_statement (sparql_buffer->connection, "move-folder-contents.rq", NULL);
+	sparql_buffer->cleanup_audio_album_discs =
+		tracker_load_statement (sparql_buffer->connection, "cleanup-audio-album-discs.rq", NULL);
+	sparql_buffer->cleanup_audio_albums =
+		tracker_load_statement (sparql_buffer->connection, "cleanup-audio-albums.rq", NULL);
+	sparql_buffer->cleanup_audio_artists =
+		tracker_load_statement (sparql_buffer->connection, "cleanup-audio-artists.rq", NULL);
 
 	G_OBJECT_CLASS (tracker_sparql_buffer_parent_class)->constructed (object);
 }
@@ -432,6 +444,55 @@ push_stmt_task (TrackerSparqlBuffer    *buffer,
 
 	task = sparql_task_data_new_stmt (file, stmt);
 	sparql_buffer_push_task (buffer, task);
+}
+
+static void
+tracker_sparql_buffer_log_statement (TrackerSparqlBuffer    *buffer,
+                                     TrackerSparqlStatement *stmt,
+                                     GFile                  *file)
+{
+	TrackerBatch *batch;
+
+	g_return_if_fail (TRACKER_IS_SPARQL_BUFFER (buffer));
+	g_return_if_fail (stmt != NULL);
+
+	if (!file)
+		file = buffer->root;
+
+	g_return_if_fail (G_IS_FILE (file));
+
+	batch = tracker_sparql_buffer_get_current_batch (buffer);
+	tracker_batch_add_statement (batch, stmt, NULL);
+
+	push_stmt_task (buffer, stmt, file);
+}
+
+void
+tracker_sparql_buffer_log_cleanup (TrackerSparqlBuffer        *buffer,
+                                   TrackerSparqlBufferCleanup  cleanup)
+{
+	g_return_if_fail (TRACKER_IS_SPARQL_BUFFER (buffer));
+
+	switch (cleanup) {
+	case TRACKER_SPARQL_BUFFER_CLEANUP_AUDIO:
+		if (!buffer->cleanup_audio_album_discs ||
+		    !buffer->cleanup_audio_albums ||
+		    !buffer->cleanup_audio_artists)
+			return;
+
+		tracker_sparql_buffer_log_statement (buffer,
+		                                     buffer->cleanup_audio_album_discs,
+		                                     NULL);
+		tracker_sparql_buffer_log_statement (buffer,
+		                                     buffer->cleanup_audio_albums,
+		                                     NULL);
+		tracker_sparql_buffer_log_statement (buffer,
+		                                     buffer->cleanup_audio_artists,
+		                                     NULL);
+		break;
+	default:
+		g_return_if_reached ();
+	}
 }
 
 static char *

src/indexer/org.freedesktop.Tracker3.Miner.Files.gresource.xml
@@ -3,6 +3,9 @@
   <gresource prefix="/org/freedesktop/Tracker3/Miner/Files">
     <file>queries/ask-file-exists.rq</file>
     <file>queries/ask-unextracted.rq</file>
+    <file>queries/cleanup-audio-album-discs.rq</file>
+    <file>queries/cleanup-audio-albums.rq</file>
+    <file>queries/cleanup-audio-artists.rq</file>
     <file>queries/delete-file.rq</file>
     <file>queries/delete-file-content.rq</file>
     <file>queries/delete-folder-contents.rq</file>

src/indexer/queries/cleanup-audio-album-discs.rq
@@ -1,0 +1,15 @@
+DELETE {
+  GRAPH tracker:Audio {
+    ?disc a rdfs:Resource .
+  }
+} WHERE {
+  GRAPH tracker:Audio {
+    ?disc a nmm:MusicAlbumDisc .
+
+    FILTER (
+      NOT EXISTS {
+        ?song nmm:musicAlbumDisc ?disc
+      }
+    )
+  }
+}

src/indexer/queries/cleanup-audio-albums.rq
@@ -1,0 +1,18 @@
+DELETE {
+  GRAPH tracker:Audio {
+    ?album a rdfs:Resource .
+  }
+} WHERE {
+  GRAPH tracker:Audio {
+    ?album a nmm:MusicAlbum .
+
+    FILTER (
+      NOT EXISTS {
+        ?disc nmm:albumDiscAlbum ?album
+      } &&
+      NOT EXISTS {
+        ?song nmm:musicAlbum ?album
+      }
+    )
+  }
+}

src/indexer/queries/cleanup-audio-artists.rq
@@ -1,0 +1,36 @@
+DELETE {
+  GRAPH tracker:Audio {
+    ?artist a rdfs:Resource .
+  }
+} WHERE {
+  GRAPH tracker:Audio {
+    ?artist a nmm:Artist .
+
+    FILTER (
+      NOT EXISTS {
+        ?album nmm:albumArtist ?artist
+      } &&
+      NOT EXISTS {
+        ?song nmm:artist ?artist
+      } &&
+      NOT EXISTS {
+        ?song nmm:composer ?artist
+      } &&
+      NOT EXISTS {
+        ?song nmm:lyricist ?artist
+      } &&
+      NOT EXISTS {
+        ?song nmm:performer ?artist
+      } &&
+      NOT EXISTS {
+        ?video nmm:director ?artist
+      } &&
+      NOT EXISTS {
+        ?video nmm:leadActor ?artist
+      } &&
+      NOT EXISTS {
+        ?video nmm:producedBy ?artist
+      }
+    )
+  }
+}

src/indexer/tracker-indexer.c
@@ -98,6 +98,7 @@ struct _TrackerIndexer {
 	guint extract_content : 1;
 	guint is_paused : 1;        /* TRUE if miner is paused */
 	guint flushing : 1;         /* TRUE if flushing SPARQL */
+	guint cleanup_audio_pending : 1;
 
 	guint status_idle_id;
 	guint resume_after_disk_full_id;
@@ -950,10 +951,16 @@ sparql_buffer_flush_cb (GObject      *object,
 	indexer->flushing = FALSE;
 
 	if (tracker_sparql_buffer_limit_reached (buffer)) {
+		if (indexer->cleanup_audio_pending) {
+			indexer->cleanup_audio_pending = FALSE;
+			tracker_sparql_buffer_log_cleanup (indexer->sparql_buffer,
+			                                   TRACKER_SPARQL_BUFFER_CLEANUP_AUDIO);
+		}
+
 		if (tracker_sparql_buffer_flush (buffer,
-						 "SPARQL buffer again full after flush",
-						 sparql_buffer_flush_cb,
-						 indexer))
+		                                 "SPARQL buffer again full after flush",
+		                                 sparql_buffer_flush_cb,
+		                                 indexer))
 			indexer->flushing = TRUE;
 	}
 
@@ -1022,6 +1029,8 @@ item_remove (TrackerIndexer *indexer,
 	}
 
 	tracker_sparql_buffer_log_delete (indexer->sparql_buffer, event->file);
+	indexer->cleanup_audio_pending = TRUE;
+
 	tracker_lru_remove (indexer->urn_lru, event->file);
 }
 
@@ -1064,6 +1073,7 @@ item_move (TrackerIndexer *indexer,
 
 		tracker_sparql_buffer_log_delete_content (indexer->sparql_buffer,
 		                                          event->file);
+		indexer->cleanup_audio_pending = TRUE;
 	}
 
 	if (tracker_indexing_tree_file_is_root (indexer->indexing_tree, event->dest_file)) {
@@ -1142,10 +1152,16 @@ miner_handle_next_item (TrackerIndexer *indexer)
 				process_stop (indexer);
 			} else {
 				/* Flush any possible pending update here */
+				if (indexer->cleanup_audio_pending) {
+					indexer->cleanup_audio_pending = FALSE;
+					tracker_sparql_buffer_log_cleanup (indexer->sparql_buffer,
+					                                   TRACKER_SPARQL_BUFFER_CLEANUP_AUDIO);
+				}
+
 				if (tracker_sparql_buffer_flush (indexer->sparql_buffer,
-								 "Queue handlers NONE",
-								 sparql_buffer_flush_cb,
-								 indexer))
+				                                 "Queue handlers NONE",
+				                                 sparql_buffer_flush_cb,
+				                                 indexer))
 					indexer->flushing = TRUE;
 			}
 		}
@@ -1178,10 +1194,16 @@ miner_handle_next_item (TrackerIndexer *indexer)
 	}
 
 	if (tracker_sparql_buffer_limit_reached (indexer->sparql_buffer)) {
+		if (indexer->cleanup_audio_pending) {
+			indexer->cleanup_audio_pending = FALSE;
+			tracker_sparql_buffer_log_cleanup (indexer->sparql_buffer,
+			                                   TRACKER_SPARQL_BUFFER_CLEANUP_AUDIO);
+		}
+
 		if (tracker_sparql_buffer_flush (indexer->sparql_buffer,
-						 "SPARQL buffer limit reached",
-						 sparql_buffer_flush_cb,
-						 indexer)) {
+		                                 "SPARQL buffer limit reached",
+		                                 sparql_buffer_flush_cb,
+		                                 indexer)) {
 			indexer->flushing = TRUE;
 		} else {
 			/* If we cannot flush, wait for the pending operations
@@ -1538,7 +1560,9 @@ indexing_tree_directory_removed (TrackerIndexingTree *indexing_tree,
 	g_autoptr (GError) error = NULL;
 	QueueForeachData foreach_data = { indexer, directory };
 
-	TRACKER_NOTE (MINER_FS_EVENTS, g_message ("  Cancelled processing pool tasks at %f\n", g_timer_elapsed (timer, NULL)));
+	TRACKER_NOTE (MINER_FS_EVENTS,
+	              g_message ("  Cancelled processing pool tasks at %f\n",
+	                         g_timer_elapsed (timer, NULL)));
 
 	tracker_indexing_tree_get_root (indexing_tree, directory, NULL, &flags);
 	conn = tracker_miner_get_connection (TRACKER_MINER (indexer));
@@ -1549,8 +1573,11 @@ indexing_tree_directory_removed (TrackerIndexingTree *indexing_tree,
 	else
 		delete_index_root (indexer, directory, batch);
 
-	if (!tracker_batch_execute (batch, NULL, &error))
+	if (!tracker_batch_execute (batch, NULL, &error)) {
 		g_warning ("Error updating indexed folder: %s", error->message);
+	} else {
+		indexer->cleanup_audio_pending = TRUE;
+	}
 
 	/* Remove anything contained in the removed directory
 	 * from all relevant processing queues.
@@ -1562,7 +1589,9 @@ indexing_tree_directory_removed (TrackerIndexingTree *indexing_tree,
 	                 (GFunc) delete_nested_queued_events,
 	                 &foreach_data);
 
-	TRACKER_NOTE (MINER_FS_EVENTS, g_message ("  Removed files at %f\n", g_timer_elapsed (timer, NULL)));
+	TRACKER_NOTE (MINER_FS_EVENTS,
+	              g_message ("  Removed files at %f\n",
+	                         g_timer_elapsed (timer, NULL)));
 	g_timer_destroy (timer);
 }
 

src/indexer/tracker-sparql-buffer.h
@@ -28,6 +28,10 @@
 
 G_BEGIN_DECLS
 
+typedef enum {
+	TRACKER_SPARQL_BUFFER_CLEANUP_AUDIO,
+} TrackerSparqlBufferCleanup;
+
 #define TRACKER_TYPE_SPARQL_BUFFER (tracker_sparql_buffer_get_type())
 G_DECLARE_FINAL_TYPE (TrackerSparqlBuffer,
                       tracker_sparql_buffer,
@@ -82,6 +86,9 @@ void tracker_sparql_buffer_log_attributes_update (TrackerSparqlBuffer *buffer,
                                                   const gchar         *content_graph,
                                                   TrackerResource     *file_resource,
                                                   TrackerResource     *graph_resource);
+
+void tracker_sparql_buffer_log_cleanup (TrackerSparqlBuffer        *buffer,
+                                        TrackerSparqlBufferCleanup  cleanup);
 
 gboolean tracker_sparql_buffer_limit_reached (TrackerSparqlBuffer *buffer);
 

commit 27641ca04fb206aa728312783a13b51f03461466

indexer: Handle NULL files differently in SPARQL buffer
When logging general cleanup statements, the file will be NULL
and for the main indexer instance the root will be NULL as well.
Avoid the fiddling in search for a GFile to attribute the cleanups
to, and handle the case of a NULL file in the single place where
it matters nowadays.

Fixes: 162034c5b ("indexer: Cleanup indirect resources")

src/indexer/tracker-sparql-buffer.c
@@ -416,7 +416,8 @@ tracker_sparql_buffer_flush_finish (TrackerSparqlBuffer  *buffer,
 				task = g_ptr_array_index (update_data->tasks, i);
 				task_file = task->file;
 				sparql = sparql_task_get_sparql (task);
-				uri = g_file_get_uri (task_file);
+				if (task_file)
+					uri = g_file_get_uri (task_file);
 
 				g_string_append_printf (str, "URI: %s\nSPARQL: %s\n",
 				                        uri, sparql);
@@ -455,11 +456,6 @@ tracker_sparql_buffer_log_statement (TrackerSparqlBuffer    *buffer,
 
 	g_return_if_fail (TRACKER_IS_SPARQL_BUFFER (buffer));
 	g_return_if_fail (stmt != NULL);
-
-	if (!file)
-		file = buffer->root;
-
-	g_return_if_fail (G_IS_FILE (file));
 
 	batch = tracker_sparql_buffer_get_current_batch (buffer);
 	tracker_batch_add_statement (batch, stmt, NULL);