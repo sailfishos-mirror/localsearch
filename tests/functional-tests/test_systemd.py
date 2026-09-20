baa2ed3ae29cc6f5aec173748dcd98f9e35f2974
Carlos Garnacho <carlosg@gnome.org>
2026-09-20 14:52:13 +0200

    tests: Comment out timing related check
    Timing in (often busy) CI runners is not reliable, avoid doing this
    timing-sensitive check that sometimes results in an error.

--- tests/functional-tests/test_systemd.py
+++ tests/functional-tests/test_systemd.py
@@ -105,7 +105,8 @@ class TestSystemd(fixtures.TrackerMinerTest):
                 f.write("Foo bar baz")
 
         elapsed = time.time() - start
-        self.assertTrue(elapsed >= 5)
+        # We should ideally test for this, but cannot due to unreliable timing in CI
+        #self.assertTrue(elapsed >= 5)
 
         # The file should be indexed as usual
         self.assertResourceExists(uri)
